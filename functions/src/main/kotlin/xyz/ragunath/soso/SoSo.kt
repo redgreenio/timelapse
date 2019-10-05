16e43ef3e4a0f2d3737517aed8828b7942e0b739
Ragunath Jawahar <ragunathjawahar@gmail.com>
2019-10-05 17:52:54 +0530

    Compute depth of a function with an if...else ladder

--- functions/src/main/kotlin/xyz/ragunath/soso/SoSo.kt
+++ functions/src/main/kotlin/xyz/ragunath/soso/SoSo.kt
@@ -1,12 +1,31 @@
 package xyz.ragunath.soso
 
+import java.util.Stack
+
 typealias Depth = Int
 
 object SoSo {
   fun depthOf(code: String): Depth {
-    return code
+    val depthStack = Stack<Depth>()
+    var maxDepth = 0
+
+    code
       .fold(0) { depth, char ->
-        if (char == '{') depth + 1 else depth
+        when (char) {
+          '{' -> {
+            depthStack.push(depth + 1)
+            if (depthStack.size > maxDepth) {
+              maxDepth = depthStack.size
+            }
+            depthStack.peek()
+          }
+
+          '}' -> depthStack.pop()
+
+          else -> depth
+        }
       }
+
+    return maxDepth
   }
 }
