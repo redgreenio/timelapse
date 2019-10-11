e959505d8add49bf99bc16d44334315ea34d3c8c
Ragunath Jawahar <ragunathjawahar@gmail.com>
2019-10-11 18:33:28 +0530

    Find start line number of a function

--- functions/src/main/kotlin/xyz/ragunath/soso/FunctionAnalyzer.kt
+++ functions/src/main/kotlin/xyz/ragunath/soso/FunctionAnalyzer.kt
@@ -23,6 +23,7 @@ fun analyze(snippet: String): Result {
   var mode = SCAN_DEPTH
   var maximumDepthCount = 0
   var lineCount = 1
+  var startLine = 0
   val depthStack = Stack<Depth>()
   for (char in chars) {
     if (char == TOKEN_NEW_LINE) {
@@ -47,6 +48,7 @@ fun analyze(snippet: String): Result {
         TOKEN_OPEN_CURLY -> {
           if (depthStack.isEmpty()) {
             maximumDepthCount = 1
+            startLine = lineCount
             depthStack.push(maximumDepthCount)
           } else {
             val depthCount = depthStack.peek() + 1
@@ -60,7 +62,7 @@ fun analyze(snippet: String): Result {
         TOKEN_CLOSE_CURLY -> {
           depthStack.pop()
           if (depthStack.isEmpty()) {
-            return Result.with(maximumDepthCount, lineCount)
+            return Result.with(maximumDepthCount, lineCount, startLine)
           }
         }
       }
@@ -69,7 +71,7 @@ fun analyze(snippet: String): Result {
     previousChar = char
   }
 
-  return Result.with(maximumDepthCount, lineCount)
+  return Result.with(maximumDepthCount, lineCount, startLine)
 }
 
 private fun isSingleLineComment(previousChar: Char, char: Char): Boolean =
--- functions/src/main/kotlin/xyz/ragunath/soso/Result.kt
+++ functions/src/main/kotlin/xyz/ragunath/soso/Result.kt
@@ -2,12 +2,13 @@ package xyz.ragunath.soso
 
 data class Result(
   val depth: Depth,
-  val length: Length
+  val length: Length,
+  val startLine: Int
 ) {
   companion object {
-    val EMPTY = with(0, 0)
+    val EMPTY = with(0, 0, 0)
 
-    fun with(depth: Int, length: Int): Result =
-      Result(depth, length)
+    fun with(depth: Int, length: Int, startLine: Int): Result =
+      Result(depth, length, startLine)
   }
 }
