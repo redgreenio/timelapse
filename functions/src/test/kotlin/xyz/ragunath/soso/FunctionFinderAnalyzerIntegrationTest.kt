5990c3707e75a08e391fa4f0282b38422fc8ea61
Ragunath Jawahar <ragunathjawahar@gmail.com>
2019-10-13 16:23:40 +0530

    Detect class functions

--- functions/src/test/kotlin/xyz/ragunath/soso/FunctionFinderAnalyzerIntegrationTest.kt
+++ functions/src/test/kotlin/xyz/ragunath/soso/FunctionFinderAnalyzerIntegrationTest.kt
@@ -31,4 +31,41 @@ class FunctionFinderAnalyzerIntegrationTest {
       )
       .inOrder()
   }
+
+  @Test
+  fun `it can detect functions nested in a class`() {
+    val fileWithClassFunctionsAndOneTopLevelFunction = """
+      package xyz.ragunath.naive.calculator
+
+      class Calculator {
+        fun add(a: Int, b: Int) {
+          return a + b
+        }
+
+        fun subtract(a: Int, b: Int) {
+          return a - b
+        }
+
+        fun multiply(a: Int, b: Int) {
+          return a * b
+        }
+      }
+
+      fun justOneTopLevelFunctionForKicks() {
+        println("Just for kicks!")
+      }
+    """.trimIndent()
+
+    assertThat(detectFunctions(fileWithClassFunctionsAndOneTopLevelFunction))
+      .hasSize(4)
+
+    assertThat(detectFunctions(fileWithClassFunctionsAndOneTopLevelFunction))
+      .containsExactly(
+        Result.with(4, 6, 1),
+        Result.with(8, 10, 1),
+        Result.with(12, 14, 1),
+        Result.with(17, 19, 1)
+      )
+      .inOrder()
+  }
 }
