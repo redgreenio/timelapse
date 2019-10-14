04a0cdbd23d89614ab9b2c13a00690adb22ce602
Ragunath Jawahar <ragunathjawahar@gmail.com>
2019-10-14 17:35:20 +0530

    Scan and find multiple Swift functions

--- functions/src/main/kotlin/xyz/ragunath/soso/swift/SwiftScanner.kt
+++ functions/src/main/kotlin/xyz/ragunath/soso/swift/SwiftScanner.kt
@@ -5,16 +5,18 @@ import xyz.ragunath.soso.PossibleFunction
 private const val KEYWORD_FUNC = "func "
 
 fun swiftScan(code: String): List<PossibleFunction> {
+  val possibleFunctions = mutableListOf<PossibleFunction>()
   val lines = code.lines()
-  val lineNumber = 1
+  var lineNumber = 1
 
   for (line in lines) {
     if (line.contains(KEYWORD_FUNC)) {
       val startIndex = line.indexOf(KEYWORD_FUNC)
       val functionName = line.substring(startIndex + KEYWORD_FUNC.length, line.indexOf("(", startIndex))
-      return listOf(PossibleFunction(functionName, lineNumber))
+      possibleFunctions.add(PossibleFunction(functionName, lineNumber))
     }
+    lineNumber++
   }
 
-  return emptyList()
+  return possibleFunctions.toList()
 }
