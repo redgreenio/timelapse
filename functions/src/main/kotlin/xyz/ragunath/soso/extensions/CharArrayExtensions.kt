43bd5de8e87c3d1dc1c8eec2561b9fb40f7f9825
Ragunath Jawahar <ragunathjawahar@gmail.com>
2019-10-15 18:40:08 +0530

    Add assertion for zero-length character arrays

--- functions/src/main/kotlin/xyz/ragunath/soso/extensions/CharArrayExtensions.kt
+++ functions/src/main/kotlin/xyz/ragunath/soso/extensions/CharArrayExtensions.kt
@@ -1,6 +1,8 @@
 package xyz.ragunath.soso.extensions
 
 fun CharArray.push(char: Char) {
+  check(this.isNotEmpty()) { "Cannot push into a zero-length array" }
+
   for (i in 0..this.size - 2) {
     this[i] = this[i + 1]
   }
