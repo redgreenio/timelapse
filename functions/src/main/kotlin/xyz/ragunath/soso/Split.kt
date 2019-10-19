08d9b03050b6963b9d9e8d1e4fabb00e1e3abe2f
Ragunath Jawahar <ragunathjawahar@gmail.com>
2019-10-19 18:49:57 +0530

    Group similar functions together

--- functions/src/main/kotlin/xyz/ragunath/soso/Split.kt
+++ functions/src/main/kotlin/xyz/ragunath/soso/Split.kt
@@ -19,24 +19,6 @@ fun split(
     .toList()
 }
 
-fun getParseResults(
-  scanner: (String) -> List<PossibleFunction>,
-  snippet: String
-): List<ParseResult> {
-  val possibleFunctions = scanner(snippet)
-  if (possibleFunctions.isEmpty()) {
-    return emptyList()
-  }
-
-  val lineNumbers = possibleFunctions.map { it.startLineNumber }
-  val functionSnippets = split(snippet, lineNumbers.first(), *lineNumbers.drop(1).toIntArray())
-  val parseResults = functionSnippets
-    .zip(lineNumbers)
-    .map { (functionSnippet, lineNumber) -> parse(functionSnippet, lineNumber) }
-
-  return possibleFunctions.zip(parseResults) { _, result -> result }
-}
-
 private fun getSplitRanges(
   splitLineNumbers: List<Int>,
   totalLines: Int
