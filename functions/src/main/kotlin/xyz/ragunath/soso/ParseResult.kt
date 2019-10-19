81c6dc545d272b4aca518fb348adf7168244c5fb
Ragunath Jawahar <ragunathjawahar@gmail.com>
2019-10-19 19:25:25 +0530

    Add `startLine` and `endLine` assertions for all factory function calls

--- functions/src/main/kotlin/xyz/ragunath/soso/ParseResult.kt
+++ functions/src/main/kotlin/xyz/ragunath/soso/ParseResult.kt
@@ -9,20 +9,26 @@ sealed class ParseResult(
 
   companion object {
     fun nothing(startLine: Int, endLine: Int): Nothing {
+      check(startLine, endLine)
       return Nothing(startLine, endLine)
     }
 
     fun wellFormedFunction(startLine: Int, endLine: Int, depth: Int): WellFormedFunction {
-      check(startLine >= 0) { "`startLine`: $startLine should be a positive integer" }
-      check(endLine >= 0) { "`endLine`: $endLine should be a positive integer" }
+      check(startLine, endLine)
       check(depth >= 0) { "`depth`: $depth should be a positive integer" }
       check(!(startLine == 0 && endLine == 0 && depth != 0)) { "`depth` must be zero for a non-existent function, but was `$depth`" }
-      check(startLine <= endLine) { "`startLine`: $startLine cannot be greater than `endLine`: $endLine" }
       return WellFormedFunction(startLine, endLine, depth)
     }
 
     fun malformedFunction(startLine: Int, endLine: Int): MalformedFunction {
+      check(startLine, endLine)
       return MalformedFunction(startLine, endLine)
+    }
+
+    private fun check(startLine: Int, endLine: Int) {
+      check(startLine >= 0) { "`startLine`: $startLine should be a positive integer" }
+      check(endLine >= 0) { "`endLine`: $endLine should be a positive integer" }
+      check(startLine <= endLine) { "`startLine`: $startLine cannot be greater than `endLine`: $endLine" }
     }
   }
 
