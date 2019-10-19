e406bc70a4ecf890cb1fd6c264468fb2b678afc2
Ragunath Jawahar <ragunathjawahar@gmail.com>
2019-10-19 21:33:52 +0530

    Replace integer constants with enums

--- functions/src/main/kotlin/xyz/ragunath/soso/swift/SwiftFunctionScanner.kt
+++ functions/src/main/kotlin/xyz/ragunath/soso/swift/SwiftFunctionScanner.kt
@@ -3,23 +3,29 @@ package xyz.ragunath.soso.swift
 import xyz.ragunath.soso.FunctionScanner
 import xyz.ragunath.soso.PossibleFunction
 import xyz.ragunath.soso.extensions.push
-
-private val KEYWORD_FUNC = "func ".toCharArray()
-
-private const val MODE_SEEK_FUNCTION = 0
-private const val MODE_SEEK_FUNCTION_NAME = 1
-private const val MODE_SEEK_OPEN_PARENTHESIS = 2
-private const val MODE_SKIP_SINGLE_LINE_COMMENT = 3
+import xyz.ragunath.soso.swift.SwiftFunctionScanner.ScanMode.SEEK_FUNCTION
+import xyz.ragunath.soso.swift.SwiftFunctionScanner.ScanMode.SEEK_FUNCTION_NAME
+import xyz.ragunath.soso.swift.SwiftFunctionScanner.ScanMode.SEEK_OPEN_PARENTHESIS
+import xyz.ragunath.soso.swift.SwiftFunctionScanner.ScanMode.SKIP_SINGLE_LINE_COMMENT
 
 class SwiftFunctionScanner : FunctionScanner {
+  private val keyword = "func ".toCharArray()
+
+  private enum class ScanMode {
+    SEEK_FUNCTION,
+    SEEK_FUNCTION_NAME,
+    SEEK_OPEN_PARENTHESIS,
+    SKIP_SINGLE_LINE_COMMENT
+  }
+
   override fun scan(snippet: String): List<PossibleFunction> {
     val possibleFunctions = mutableListOf<PossibleFunction>()
-    val buffer = CharArray(KEYWORD_FUNC.size)
+    val buffer = CharArray(keyword.size)
     val snippetChars = snippet.toCharArray()
     var lineNumber = 1
     var possibleLineNumber = lineNumber
-    var mode = MODE_SEEK_FUNCTION
-    var previousMode = MODE_SEEK_FUNCTION
+    var mode = SEEK_FUNCTION
+    var previousMode = SEEK_FUNCTION
     val functionNameChars = mutableListOf<Char>()
 
     for (char in snippetChars) {
@@ -27,37 +33,37 @@ class SwiftFunctionScanner : FunctionScanner {
 
       if (buffer[buffer.size - 2] == '/' && buffer[buffer.size - 1] == '/') {
         previousMode = mode
-        mode = MODE_SKIP_SINGLE_LINE_COMMENT
+        mode = SKIP_SINGLE_LINE_COMMENT
       }
 
-      if (mode == MODE_SEEK_FUNCTION) {
-        if (buffer.contentEquals(KEYWORD_FUNC)) {
+      if (mode == SEEK_FUNCTION) {
+        if (buffer.contentEquals(keyword)) {
           possibleLineNumber = lineNumber
-          mode = MODE_SEEK_FUNCTION_NAME
+          mode = SEEK_FUNCTION_NAME
         }
       }
 
-      if (mode == MODE_SEEK_FUNCTION_NAME && char != ' ' && char != '(') {
+      if (mode == SEEK_FUNCTION_NAME && char != ' ' && char != '(') {
         functionNameChars.add(char)
       } else if (char == '(') {
-        mode = MODE_SEEK_OPEN_PARENTHESIS
+        mode = SEEK_OPEN_PARENTHESIS
       }
 
-      if (mode == MODE_SEEK_OPEN_PARENTHESIS && char == '}') {
-        mode = MODE_SEEK_FUNCTION
+      if (mode == SEEK_OPEN_PARENTHESIS && char == '}') {
+        mode = SEEK_FUNCTION
         functionNameChars.clear()
       }
 
-      if (mode == MODE_SEEK_OPEN_PARENTHESIS && char == '{' && functionNameChars.isNotEmpty()) {
+      if (mode == SEEK_OPEN_PARENTHESIS && char == '{' && functionNameChars.isNotEmpty()) {
         val possibleFunctionName = functionNameChars.joinToString("")
         possibleFunctions.add(PossibleFunction(possibleFunctionName, possibleLineNumber))
-        mode = MODE_SEEK_FUNCTION
+        mode = SEEK_FUNCTION
         functionNameChars.clear()
       }
 
       if (char == '\n') {
         lineNumber++
-        if (mode == MODE_SKIP_SINGLE_LINE_COMMENT) {
+        if (mode == SKIP_SINGLE_LINE_COMMENT) {
           mode = previousMode
         }
       }
