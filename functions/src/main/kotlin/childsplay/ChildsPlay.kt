c445b80a1cbaa8661826a3b23b519afe5c78a72f
Ragunath Jawahar <ragunathjawahar@gmail.com>
2019-10-20 20:59:14 +0530

    Log files that do not have any functions in them

--- functions/src/main/kotlin/childsplay/ChildsPlay.kt
+++ functions/src/main/kotlin/childsplay/ChildsPlay.kt
@@ -13,33 +13,40 @@ import kotlin.streams.asSequence
 
 fun main() {
   val startTime = System.currentTimeMillis()
+  val logger = LogManager.getLogger()
+
   val projects = arrayOf(
     "pechu-thiramai",
     "Alamofire",
     "RxSwift",
-    "TelegramSwift",
+    "kaathadi-ios",
     "swift",
-    "kaathadi-ios"
+    "TelegramSwift"
   )
 
   val projectPath = Paths.get("/Users/ragunathjawahar/OtherProjects/${projects[projects.size - 1]}")
-  val parseResults = Files
+  val sourceFiles = Files
     .find(projectPath, Integer.MAX_VALUE, BiPredicate { _, fileAttributes -> fileAttributes.isRegularFile })
     .asSequence()
     .filter { it.fileName.toString().endsWith(".swift") }
     .filter { !it.toAbsolutePath().toString().contains("/Pods/") }
+    .toList()
+
+  val parseResults = sourceFiles
+    .asSequence()
+    .onEach { logger.info("Inspecting file ${it.fileName}") }
     .map { Files.readAllBytes(it) }
     .map { String(it) }
     .map { snippet -> getParseResults(SwiftFunctionScanner::scan, snippet) }
+    .onEach { if (it.isEmpty()) logger.warn("No functions found, suspicious. PTAL?") }
     .filter { it.isNotEmpty() }
-    .asSequence()
-    .toList()
     .flatten()
 
   val endTime = System.currentTimeMillis()
-  val logger = LogManager.getLogger()
-  logger.info("Est. well-formed functions: ${parseResults.filterIsInstance<WellFormedFunction>().size}")
-  logger.info("Est. malformed functions: ${parseResults.filterIsInstance<MalformedFunction>().size}")
-  logger.info("Est. non-functions: ${parseResults.filterIsInstance<Nothing>().size}")
+  logger.info("Total number of scanned files: ${sourceFiles.size}")
+  val parseResultsList = parseResults.toList()
+  logger.info("Est. well-formed functions: ${parseResultsList.filterIsInstance<WellFormedFunction>().size}")
+  logger.info("Est. malformed functions: ${parseResultsList.filterIsInstance<MalformedFunction>().size}")
+  logger.info("Est. non-functions: ${parseResultsList.filterIsInstance<Nothing>().size}")
   logger.info("Analysis took: ${endTime - startTime}ms")
 }
