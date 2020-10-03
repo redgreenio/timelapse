d38c3320c6caa7da1b19c8648d889f48ce22a798
Ragunath Jawahar <ragunath@redgreen.io>
2020-10-03 14:29:30 +0530

    build: setup project

---
+++ build.gradle.kts
@@ -0,0 +1,34 @@
+plugins {
+  kotlin("jvm") version "1.4.0"
+}
+
+group = "io.redgreen"
+version = "0.1.0-SNAPSHOT"
+
+repositories {
+  mavenCentral()
+}
+
+dependencies {
+  implementation(kotlin("stdlib-jdk8"))
+
+  testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
+  testImplementation("org.junit.jupiter:junit-jupiter-params:5.6.0")
+  testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.0")
+
+  testImplementation("com.google.truth:truth:1.0.1")
+}
+
+tasks {
+  compileKotlin {
+    kotlinOptions.jvmTarget = "1.8"
+  }
+
+  compileTestKotlin {
+    kotlinOptions.jvmTarget = "1.8"
+  }
+
+  test {
+    useJUnitPlatform()
+  }
+}
---
+++ src/test/kotlin/CanaryTest.kt
@@ -0,0 +1,10 @@
+import com.google.common.truth.Truth.assertThat
+import org.junit.jupiter.api.Test
+
+class CanaryTest {
+  @Test
+  fun `test environment is setup`() {
+    assertThat(true)
+      .isTrue()
+  }
+}
