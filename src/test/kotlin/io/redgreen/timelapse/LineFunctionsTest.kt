97536e165432ea55883abe4b658e695697ee611f
Ragunath Jawahar <ragunath@redgreen.io>
2020-10-03 18:52:12 +0530

    test: vertical line slope (parallel to y-axis)

--- src/test/kotlin/io/redgreen/timelapse/LineFunctionsTest.kt
+++ src/test/kotlin/io/redgreen/timelapse/LineFunctionsTest.kt
@@ -7,11 +7,17 @@ import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.ArgumentsProvider
 import org.junit.jupiter.params.provider.ArgumentsSource
+import org.junit.jupiter.params.provider.ArgumentsSources
 import java.util.stream.Stream
+import kotlin.Double.Companion.NEGATIVE_INFINITY
+import kotlin.Double.Companion.POSITIVE_INFINITY
 
 class LineFunctionsTest {
   @ParameterizedTest
-  @ArgumentsSource(HorizontalLineArgumentsProvider::class)
+  @ArgumentsSources(
+    ArgumentsSource(HorizontalLineArgumentsProvider::class),
+    ArgumentsSource(VerticalLineArgumentsProvider::class),
+  )
   fun `it should calculate slope`(
     coordinates: Tuple5<Int, Int, Int, Int, Double>
   ) {
@@ -29,7 +35,7 @@ class LineFunctionsTest {
 
 class HorizontalLineArgumentsProvider : ArgumentsProvider {
   override fun provideArguments(context: ExtensionContext): Stream<out Arguments> {
-    val coordinates = listOf(
+    return listOf(
       // Lines passing through origin (left -> right)
       Tuple5(0, 0, 1, 0, 0.0),
       Tuple5(-1, 0, 0, 0, 0.0),
@@ -49,8 +55,35 @@ class HorizontalLineArgumentsProvider : ArgumentsProvider {
       Tuple5(2, -4, 1, -4, -0.0),
       Tuple5(1, -4, -1, -4, -0.0),
       Tuple5(-1, -4, -2, -4, -0.0),
-    )
-
-    return coordinates.map { Arguments.of(it) }.stream()
+    ).toArgumentsStream()
   }
 }
+
+class VerticalLineArgumentsProvider : ArgumentsProvider {
+  override fun provideArguments(context: ExtensionContext?): Stream<out Arguments> {
+    return listOf(
+      // Lines passing through origin (top -> bottom)
+      Tuple5(0, 1, 0, 0, NEGATIVE_INFINITY),
+      Tuple5(0, 0, 0, -1, NEGATIVE_INFINITY),
+      Tuple5(0, 1, 0, -1, NEGATIVE_INFINITY),
+
+      // Lines passing through origin (bottom -> top)
+      Tuple5(0, 0, 0, 1, POSITIVE_INFINITY),
+      Tuple5(0, -1, 0, 0, POSITIVE_INFINITY),
+      Tuple5(0, -1, 0, 1, POSITIVE_INFINITY),
+
+      // Lines parallel to y-axis (top -> bottom)
+      Tuple5(2, 4, 2, 2, NEGATIVE_INFINITY),
+      Tuple5(2, 1, 2, -1, NEGATIVE_INFINITY),
+      Tuple5(-2, -1, -2, -2, NEGATIVE_INFINITY),
+
+      // Lines parallel to y-axis (bottom -> top)
+      Tuple5(2, 2, 2, 4, POSITIVE_INFINITY),
+      Tuple5(2, -1, 2, 1, POSITIVE_INFINITY),
+      Tuple5(-2, -2, -2, -1, POSITIVE_INFINITY),
+    ).toArgumentsStream()
+  }
+}
+
+private fun <T> List<T>.toArgumentsStream(): Stream<out Arguments> =
+  this.map { Arguments.of(it) }.stream()
