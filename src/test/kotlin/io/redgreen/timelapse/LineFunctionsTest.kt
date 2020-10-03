2c62c42cf671b4e11343b674909724080833ee02
Ragunath Jawahar <ragunath@redgreen.io>
2020-10-03 16:44:23 +0530

    refactor: replace `MethodSource` with `ArgumentsSource`

--- src/test/kotlin/io/redgreen/timelapse/LineFunctionsTest.kt
+++ src/test/kotlin/io/redgreen/timelapse/LineFunctionsTest.kt
@@ -2,55 +2,55 @@ package io.redgreen.timelapse
 
 import arrow.core.Tuple5
 import com.google.common.truth.Truth.assertThat
-import org.junit.jupiter.api.Nested
-import org.junit.jupiter.api.TestInstance
-import org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS
+import org.junit.jupiter.api.extension.ExtensionContext
 import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.MethodSource
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import java.util.stream.Stream
 
 class LineFunctionsTest {
-  @Nested
-  @TestInstance(PER_CLASS)
-  inner class HorizontalLine {
-    @ParameterizedTest
-    @MethodSource("horizontalLineCoordinates")
-    fun `it should calculate slope for horizontal lines`(
-      coordinates: Tuple5<Int, Int, Int, Int, Double>
-    ) {
-      // given
-      val (x1, y1, x2, y2, expectedSlope) = coordinates
+  @ParameterizedTest
+  @ArgumentsSource(HorizontalLineArgumentsProvider::class)
+  fun `it should calculate slope`(
+    coordinates: Tuple5<Int, Int, Int, Int, Double>
+  ) {
+    // given
+    val (x1, y1, x2, y2, expectedSlope) = coordinates
 
-      // when
-      val slope = calculateSlope(x1, y1, x2, y2)
+    // when
+    val actualSlope = calculateSlope(x1, y1, x2, y2)
 
-      // then
-      assertThat(slope)
-        .isEqualTo(expectedSlope)
-    }
+    // then
+    assertThat(actualSlope)
+      .isEqualTo(expectedSlope)
+  }
+}
 
-    @Suppress("unused") // function is referenced in @MethodSource
-    fun horizontalLineCoordinates(): List<Tuple5<Int, Int, Int, Int, Double>> {
-      return listOf(
-        // Lines passing through origin (left -> right)
-        Tuple5(0, 0, 1, 0, 0.0),
-        Tuple5(-1, 0, 0, 0, 0.0),
-        Tuple5(-1, 0, 1, 0, 0.0),
+class HorizontalLineArgumentsProvider : ArgumentsProvider {
+  override fun provideArguments(context: ExtensionContext): Stream<out Arguments> {
+    val coordinates = listOf(
+      // Lines passing through origin (left -> right)
+      Tuple5(0, 0, 1, 0, 0.0),
+      Tuple5(-1, 0, 0, 0, 0.0),
+      Tuple5(-1, 0, 1, 0, 0.0),
 
-        // Lines passing through origin (right -> left)
-        Tuple5(0, 0, -1, 0, -0.0),
-        Tuple5(1, 0, 0, 0, -0.0),
-        Tuple5(1, 0, -1, 0, -0.0),
+      // Lines passing through origin (right -> left)
+      Tuple5(0, 0, -1, 0, -0.0),
+      Tuple5(1, 0, 0, 0, -0.0),
+      Tuple5(1, 0, -1, 0, -0.0),
 
-        // Lines away from origin (left -> right)
-        Tuple5(1, 4, 2, 4, 0.0),
-        Tuple5(-1, 4, 1, 4, 0.0),
-        Tuple5(-2, 4, -1, 4, 0.0),
+      // Lines parallel to x-axis (left -> right)
+      Tuple5(1, 4, 2, 4, 0.0),
+      Tuple5(-1, 4, 1, 4, 0.0),
+      Tuple5(-2, 4, -1, 4, 0.0),
 
-        // Lines away from origin (right -> left)
-        Tuple5(2, -4, 1, -4, -0.0),
-        Tuple5(1, -4, -1, -4, -0.0),
-        Tuple5(-1, -4, -2, -4, -0.0),
-      )
-    }
+      // Lines parallel to x-axis (right -> left)
+      Tuple5(2, -4, 1, -4, -0.0),
+      Tuple5(1, -4, -1, -4, -0.0),
+      Tuple5(-1, -4, -2, -4, -0.0),
+    )
+
+    return coordinates.map { Arguments.of(it) }.stream()
   }
 }
