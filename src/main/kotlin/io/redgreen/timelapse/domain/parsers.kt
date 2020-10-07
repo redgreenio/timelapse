1e78f41abbfc75c2581fcf89c190b5a5ce842e53
Ragunath Jawahar <ragunath@redgreen.io>
2020-10-07 10:10:40 +0530

    refactor: extract functions from git follow parser

--- src/main/kotlin/io/redgreen/timelapse/domain/parsers.kt
+++ src/main/kotlin/io/redgreen/timelapse/domain/parsers.kt
@@ -2,29 +2,35 @@ package io.redgreen.timelapse.domain
 
 import java.util.regex.Pattern
 
+private const val INSERTIONS_REGEX = "\\d+ insertions?\\(\\+\\)"
+private const val DELETIONS_REGEX = "\\d+ deletions?\\(-\\)"
+
 fun parseGitFollowOutput(output: String): Change {
   val lines = output.split("\n")
+
   val commitIdMessageLine = lines[0]
-  val commitAndMessageSeparatorIndex = commitIdMessageLine.indexOf(' ')
-  val commitId = commitIdMessageLine.substring(0, commitAndMessageSeparatorIndex)
-  val message = commitIdMessageLine.substring(commitAndMessageSeparatorIndex + 1)
-
   val insertionsDeletionsLine = lines[2]
-  val insertionsPattern = Pattern.compile("\\d+ insertions?\\(\\+\\)")
-  val insertionsMatcher = insertionsPattern.matcher(insertionsDeletionsLine)
-  val insertions = if (insertionsMatcher.find()) {
-    insertionsMatcher.group().split(" ")[0].toInt()
-  } else {
-    0
-  }
 
-  val deletionsPattern = Pattern.compile("\\d+ deletions?\\(-\\)")
-  val deletionsMatcher = deletionsPattern.matcher(insertionsDeletionsLine)
-  val deletions = if (deletionsMatcher.find()) {
-    deletionsMatcher.group().split(" ")[0].toInt()
-  } else {
-    0
-  }
+  val (commitId, message) = commitIdMessageLine.splitIntoTwo(' ')
+  val insertions = insertionsDeletionsLine.extractIntFromRegex(INSERTIONS_REGEX)
+  val deletions = insertionsDeletionsLine.extractIntFromRegex(DELETIONS_REGEX)
 
   return Change(commitId, message, insertions, deletions)
+}
+
+private fun String.splitIntoTwo(delimiter: Char): Pair<String, String> {
+  val delimiterIndex = this.indexOf(delimiter)
+  val first = this.substring(0, delimiterIndex)
+  val second = this.substring(delimiterIndex + 1)
+  return first to second
+}
+
+private fun String.extractIntFromRegex(regex: String): Int {
+  val pattern = Pattern.compile(regex)
+  val matcher = pattern.matcher(this)
+  return if (matcher.find()) {
+    matcher.group().splitIntoTwo(' ').first.toInt()
+  } else {
+    0
+  }
 }
