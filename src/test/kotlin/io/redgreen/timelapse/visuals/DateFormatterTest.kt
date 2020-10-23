427d1afc7dda84e3742b85b3b1a0a4f7cb7dc339
Ragunath Jawahar <ragunath@redgreen.io>
2020-10-23 15:48:08 +0530

    fix: move from deprecated `java.util.Date` APIs

--- src/test/kotlin/io/redgreen/timelapse/visuals/DateFormatterTest.kt
+++ src/test/kotlin/io/redgreen/timelapse/visuals/DateFormatterTest.kt
@@ -2,16 +2,18 @@ package io.redgreen.timelapse.visuals
 
 import com.google.common.truth.Truth.assertThat
 import org.junit.jupiter.api.Test
+import java.time.LocalDateTime
+import java.time.Month
+import java.time.Month.OCTOBER
+import java.time.ZoneId
 import java.util.Date
 
 class DateFormatterTest {
   @Test
   fun `it should show author and commit date and time`() {
     // given
-    val year = 2020 - 1900
-    val october = 9
-    val authoredDate = Date(year, october, 20, 22, 49)
-    val committedDate = Date(year, october, 20, 22, 55)
+    val authoredDate = toUtilDate(2020, OCTOBER, 20, 22, 49)
+    val committedDate = toUtilDate(2020, OCTOBER, 20, 22, 55)
 
     // when
     val formattedDateString = formatDate(authoredDate, committedDate)
@@ -19,5 +21,18 @@ class DateFormatterTest {
     // then
     assertThat(formattedDateString)
       .isEqualTo("Tue, 20 Oct 2020 22:49, committed on Tue, 20 Oct 2020 22:55")
+  }
+
+  @Suppress("SameParameterValue")
+  private fun toUtilDate(
+    year: Int,
+    month: Month,
+    dayOfMonth: Int,
+    hour: Int,
+    minute: Int
+  ): Date {
+    val localDateTime = LocalDateTime.of(year, month, dayOfMonth, hour, minute)
+    return Date
+      .from(localDateTime.atZone(ZoneId.systemDefault()).toInstant())
   }
 }
