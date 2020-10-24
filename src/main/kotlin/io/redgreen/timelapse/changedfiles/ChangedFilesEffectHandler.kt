d98d845b46ab11d0f7e5dfe595c3df2aff4066ca
Ragunath Jawahar <ragunath@redgreen.io>
2020-10-24 08:47:31 +0530

    improvement: log exceptions for get changed files in effect handler

--- src/main/kotlin/io/redgreen/timelapse/changedfiles/ChangedFilesEffectHandler.kt
+++ src/main/kotlin/io/redgreen/timelapse/changedfiles/ChangedFilesEffectHandler.kt
@@ -3,26 +3,49 @@ package io.redgreen.timelapse.changedfiles
 import com.spotify.mobius.rx3.RxMobius
 import io.reactivex.rxjava3.core.ObservableTransformer
 import io.redgreen.timelapse.changedfiles.contracts.ReadingAreaContract
+import io.redgreen.timelapse.vcs.ChangedFile
 import io.redgreen.timelapse.vcs.VcsRepositoryService
+import org.slf4j.LoggerFactory
 
 class ChangedFilesEffectHandler private constructor() {
   companion object {
+    private val logger = LoggerFactory.getLogger(ChangedFilesEffectHandler::class.java)
+
     fun from(
       vcsRepositoryService: VcsRepositoryService,
       readingAreaContract: ReadingAreaContract
     ): ObservableTransformer<ChangedFilesEffect, ChangedFilesEvent> {
       return RxMobius
         .subtypeEffectHandler<ChangedFilesEffect, ChangedFilesEvent>()
-        .addTransformer(GetChangedFiles::class.java) { getChangedFilesEvents ->
-          getChangedFilesEvents
-            .flatMapSingle { vcsRepositoryService.getChangedFiles(it.commitId) }
-            .map { if (it.size == 1) NoOtherFilesChanged else SomeMoreFilesChanged(changedFiles = it) }
-            .onErrorReturn { GettingChangedFilesFailed }
-        }
-        .addConsumer(ShowDiff::class.java) { (commitId, changedFile) ->
-          readingAreaContract.showDiff(commitId, changedFile)
-        }
+        .addTransformer(GetChangedFiles::class.java, getChangedFilesTransformer(vcsRepositoryService))
+        .addConsumer(ShowDiff::class.java) { showDiff -> realizeShowDiffEffect(showDiff, readingAreaContract) }
         .build()
+    }
+
+    private fun getChangedFilesTransformer(
+      repositoryService: VcsRepositoryService
+    ): ObservableTransformer<GetChangedFiles, ChangedFilesEvent> {
+      return ObservableTransformer { getChangedFilesEvents ->
+        getChangedFilesEvents
+          .flatMapSingle { event -> repositoryService.getChangedFiles(event.commitId) }
+          .map(::toChangedFilesEvent)
+          .doOnError { logger.debug("Get changed files failed.", it) }
+          .onErrorReturn { GettingChangedFilesFailed }
+      }
+    }
+
+    private fun toChangedFilesEvent(changedFiles: List<ChangedFile>): ChangedFilesEvent = if (changedFiles.size == 1) {
+      NoOtherFilesChanged
+    } else {
+      SomeMoreFilesChanged(changedFiles)
+    }
+
+    private fun realizeShowDiffEffect(
+      showDiff: ShowDiff,
+      readingAreaContract: ReadingAreaContract
+    ) {
+      val (commitId, changedFile) = showDiff
+      readingAreaContract.showDiff(commitId, changedFile)
     }
   }
 }
