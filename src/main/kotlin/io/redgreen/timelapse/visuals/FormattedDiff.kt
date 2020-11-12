d271b670aaef83f343720049cef5717d11972ed5
Ragunath Jawahar <ragunath@redgreen.io>
2020-11-12 07:10:39 +0530

    refactor: extract constants and make file more readable

--- src/main/kotlin/io/redgreen/timelapse/visuals/FormattedDiff.kt
+++ src/main/kotlin/io/redgreen/timelapse/visuals/FormattedDiff.kt
@@ -7,27 +7,41 @@ import io.redgreen.timelapse.visuals.DiffSpan.Insertion
 import io.redgreen.timelapse.visuals.DiffSpan.Marker
 import io.redgreen.timelapse.visuals.DiffSpan.Unmodified
 
-private const val DIFF_TYPE_DELETED = "deleted"
-private const val DIFF_TYPE_NEW_FILE = "new file"
-
 class FormattedDiff private constructor(val spans: List<DiffSpan>) {
   companion object {
-    private val contentsEmpty = listOf(Unmodified(""))
+    private const val DIFF_TYPE_LINE_INDEX = 1
+    private const val DIFF_TYPE_NEW_FILE = "new file"
+    private const val DIFF_TYPE_DELETED = "deleted"
+
+    private const val HEADER_LINES_COUNT_NEW_OR_DELETED_FILE = 5
+    private const val HEADER_LINES_COUNT_MODIFIED_FILE = 4
+
+    private const val EMPTY_STRING = ""
+
+    private val contentsEmpty = listOf(Unmodified(EMPTY_STRING))
 
     fun from(rawDiff: String): FormattedDiff {
-      val diffTypeLine = rawDiff.lines().take(2).last()
-      val newOrDeletedFile = with(diffTypeLine) { startsWith(DIFF_TYPE_NEW_FILE) || startsWith(DIFF_TYPE_DELETED) }
-      val skipLines = if (newOrDeletedFile) { 4 } else { 3 }
+      val rawDiffLines = rawDiff.lines()
+      val headerLineCount = headerLineCount(rawDiffLines[DIFF_TYPE_LINE_INDEX])
 
-      val spans = rawDiff
-        .lines()
-        .filterIndexed { index, _ -> index > skipLines }
+      val spans = rawDiffLines
+        .drop(headerLineCount)
         .map(::toSpan)
 
       return if (spans == contentsEmpty) {
         FormattedDiff(listOf(ContentsEmpty))
       } else {
         FormattedDiff(spans)
+      }
+    }
+
+    private fun headerLineCount(diffTypeLine: String): Int {
+      val newOrDeletedFile = diffTypeLine.startsWith(DIFF_TYPE_NEW_FILE) ||
+          diffTypeLine.startsWith(DIFF_TYPE_DELETED)
+      return if (newOrDeletedFile) {
+        HEADER_LINES_COUNT_NEW_OR_DELETED_FILE
+      } else {
+        HEADER_LINES_COUNT_MODIFIED_FILE
       }
     }
 
