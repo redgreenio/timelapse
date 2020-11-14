4f88dcf8d61b6250b5eaa297df63bb1d761e0644
Ragunath Jawahar <ragunath@redgreen.io>
2020-11-14 07:03:20 +0530

    test: add canary test for approvals

--- src/test/kotlin/CanaryTests.kt
+++ src/test/kotlin/CanaryTests.kt
@@ -1,10 +1,16 @@
 import com.google.common.truth.Truth.assertThat
+import org.approvaltests.Approvals
 import org.junit.jupiter.api.Test
 
-class CanaryTest {
+class CanaryTests {
   @Test
   fun `test environment is setup`() {
     assertThat(true)
       .isTrue()
+  }
+
+  @Test
+  fun `approvals is setup`() {
+    Approvals.verify("Hello, world!")
   }
 }
