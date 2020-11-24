36c1533cf13dde6eb0847bf2196f4d358d5f026f
Ragunath Jawahar <ragunath@redgreen.io>
2020-11-24 21:09:26 +0530

    refactor: remove extraneous annotation class

--- src/main/kotlin/io/redgreen/timelapse/changedfiles/contracts/ReadingAreaContract.kt
+++ src/main/kotlin/io/redgreen/timelapse/changedfiles/contracts/ReadingAreaContract.kt
@@ -1,10 +1,7 @@
 package io.redgreen.timelapse.changedfiles.contracts
 
-import io.redgreen.timelapse.TimelapseApp
-import io.redgreen.timelapse.contracts.FulfilledBy
 import io.redgreen.timelapse.vcs.ChangedFile
 
-@FulfilledBy(TimelapseApp::class)
 interface ReadingAreaContract {
   fun showChangedFileDiff(commitId: String, changedFile: ChangedFile)
 }
