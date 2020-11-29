c78f8ff8a462451cd1b3b511466a9cbd3bfbd09d
Ragunath Jawahar <ragunath@redgreen.io>
2020-11-29 18:04:41 +0530

    improvement: add a new rounded corner label component

--- visuals-demo/src/main/kotlin/io/redgreen/visuals/demo/Scenes.kt
+++ visuals-demo/src/main/kotlin/io/redgreen/visuals/demo/Scenes.kt
@@ -2,8 +2,22 @@ package io.redgreen.visuals.demo
 
 import io.redgreen.timelapse.visuals.AreaChart
 import io.redgreen.timelapse.visuals.Commit
+import io.redgreen.timelapse.visuals.RoundedCornerLabel
+import javafx.geometry.Insets
 import javafx.scene.Scene
+import javafx.scene.control.Label
+import javafx.scene.control.Tooltip
+import javafx.scene.layout.Background
+import javafx.scene.layout.BackgroundFill
+import javafx.scene.layout.CornerRadii
+import javafx.scene.layout.HBox
 import javafx.scene.layout.Pane
+import javafx.scene.layout.VBox
+import javafx.scene.paint.Color
+import javafx.scene.paint.Color.LIGHTGREY
+import javafx.scene.paint.Color.WHITESMOKE
+import javafx.scene.text.Text
+import javafx.scene.text.TextFlow
 
 private const val areaChartWidth = 800
 private const val areaChartHeight = 150
@@ -22,10 +36,45 @@ private val areaChart = AreaChart().apply {
   commits = sampleCommits
 }
 
+@Suppress("unused")
 internal fun areaChartScene(): Scene {
   return Scene(Pane().apply {
     children.add(areaChart)
     areaChart.prefWidthProperty().bind(widthProperty())
     areaChart.prefHeightProperty().bind(heightProperty())
   }, areaChartWidth.toDouble(), areaChartHeight.toDouble())
+}
+
+internal fun fileSummaryBar(): Scene {
+  return Scene(VBox().apply {
+    val pane = this
+    background = Background(BackgroundFill(LIGHTGREY, CornerRadii.EMPTY, Insets.EMPTY))
+
+    val fileNameLabel = Label("Filter.swift").apply {
+      style = "-fx-font-weight: bold;"
+      padding = Insets(0.0, 4.0, 0.0, 0.0)
+    }
+
+    children.addAll(
+      HBox(
+        fileNameLabel,
+        RoundedCornerLabel(Color.rgb(0xff, 0xdc, 0xe0), "-5").apply { tooltip = Tooltip("5 deletions") },
+        RoundedCornerLabel(Color.rgb(0xbe, 0xf5, 0xcb), "+52").apply { tooltip = Tooltip("52 insertions") },
+        RoundedCornerLabel(WHITESMOKE, "1").apply { tooltip = Tooltip("1 more file changed") },
+      ).apply {
+        spacing = 4.0
+        padding = Insets(8.0, 8.0, 0.0, 8.0)
+        prefWidthProperty().bind(pane.widthProperty())
+      },
+
+      TextFlow(
+        Text("e37ae8f5").apply {
+          fill = Color.CORNFLOWERBLUE
+          style = "-fx-font-weight: bold;"
+          Tooltip.install(this, Tooltip("e37ae8f566215aa613341d17171b3b916e3f6a0a (click to copy)"))
+        },
+        Text(" • Make ignore element return Observable<New>")
+      ).apply { padding = Insets(2.0, 8.0, 0.0, 8.0) }
+    )
+  }, 600.0, 52.0)
 }
---
+++ visuals/src/main/kotlin/io/redgreen/timelapse/visuals/RoundedCornerLabel.kt
@@ -0,0 +1,30 @@
+package io.redgreen.timelapse.visuals
+
+import javafx.geometry.Insets
+import javafx.geometry.Insets.EMPTY
+import javafx.geometry.Pos
+import javafx.scene.control.Label
+import javafx.scene.layout.Background
+import javafx.scene.layout.BackgroundFill
+import javafx.scene.layout.CornerRadii
+import javafx.scene.paint.Color
+
+class RoundedCornerLabel(
+  backgroundColor: Color,
+  text: String
+): Label(text) {
+  companion object {
+    private const val MIN_WIDTH = 24.0
+    private const val CORNER_RADII = 3.5
+    private const val PADDING = 4.0
+  }
+
+  init {
+    minWidth = MIN_WIDTH
+    alignment = Pos.CENTER
+
+    val cornerRadii = CornerRadii(CORNER_RADII)
+    background = Background(BackgroundFill(backgroundColor, cornerRadii, EMPTY))
+    padding = Insets(0.0, PADDING, 0.0, PADDING)
+  }
+}
