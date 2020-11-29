1661a4d96f86cef62a2110c41d31915c42215dd8
Ragunath Jawahar <ragunath@redgreen.io>
2020-11-29 17:12:19 +0530

    refactor: extract visual demo module scene to a separate file

---
+++ visuals-demo/src/main/kotlin/io/redgreen/visuals/demo/Scenes.kt
@@ -0,0 +1,31 @@
+package io.redgreen.visuals.demo
+
+import io.redgreen.timelapse.visuals.AreaChart
+import io.redgreen.timelapse.visuals.Commit
+import javafx.scene.Scene
+import javafx.scene.layout.Pane
+
+private const val areaChartWidth = 800
+private const val areaChartHeight = 150
+
+private val sampleCommits = listOf(
+  Commit(5, 0),
+  Commit(1, 0),
+  Commit(1, 4),
+  Commit(2, 0),
+  Commit(0, 1),
+  Commit(1, 0),
+  Commit(1, 0),
+)
+
+private val areaChart = AreaChart().apply {
+  commits = sampleCommits
+}
+
+internal fun areaChartScene(): Scene {
+  return Scene(Pane().apply {
+    children.add(areaChart)
+    areaChart.prefWidthProperty().bind(widthProperty())
+    areaChart.prefHeightProperty().bind(heightProperty())
+  }, areaChartWidth.toDouble(), areaChartHeight.toDouble())
+}
--- visuals-demo/src/main/kotlin/io/redgreen/visuals/demo/VisualsDemoApp.kt
+++ visuals-demo/src/main/kotlin/io/redgreen/visuals/demo/VisualsDemoApp.kt
@@ -1,34 +1,13 @@
 package io.redgreen.visuals.demo
 
-import io.redgreen.timelapse.visuals.AreaChart
-import io.redgreen.timelapse.visuals.Commit
 import javafx.application.Application
-import javafx.scene.Scene
-import javafx.scene.layout.Pane
 import javafx.stage.Stage
 
 class VisualsDemoApp : Application() {
   override fun start(primaryStage: Stage) {
-    val sampleCommits = listOf(
-      Commit(1, 1),
-      Commit(2, 0),
-    )
-
-    val areaChart = AreaChart().apply {
-      commits = sampleCommits
-    }
-
-    val width = 800.0
-    val height = 150.0
-
     with(primaryStage) {
       title = "Visuals Demo"
-
-      scene = Scene(Pane().apply {
-        children.add(areaChart)
-        areaChart.prefWidthProperty().bind(widthProperty())
-        areaChart.prefHeightProperty().bind(heightProperty())
-      }, width, height)
+      scene = areaChartScene()
       show()
     }
   }
