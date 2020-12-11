e3aa2b204104942b92c5e98077ac260f563c1dc5
Ragunath Jawahar <ragunath@redgreen.io>
2020-12-11 18:59:24 +0530

    fix: bad JavaFx font smoothing on Mac OS X

--- app/src/main/kotlin/io/redgreen/timelapse/TimelapseApp.kt
+++ app/src/main/kotlin/io/redgreen/timelapse/TimelapseApp.kt
@@ -277,6 +277,9 @@ class TimelapseApp : Application(), ReadingAreaContract, FileSelectionListener {
 
   override fun init() {
     super.init()
+    // See https://github.com/JabRef/jabref/issues/3295
+    System.setProperty("prism.lcdtext", "false")
+
     Sentry.init { options ->
       options.dsn = "https://9ccbeb20047c42e49a9fbe6094cd9896@o483785.ingest.sentry.io/5536123"
     }
