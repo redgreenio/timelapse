e388239d3869b67dfde228461fc991db3b447b2c
Ragunath Jawahar <ragunath@redgreen.io>
2021-03-01 10:18:34 +0530

    refactor: reduce complexity of compare function

--- app/src/main/kotlin/redgreen/dawn/affectedfiles/view/model/FileCellComparator.kt
+++ app/src/main/kotlin/redgreen/dawn/affectedfiles/view/model/FileCellComparator.kt
@@ -8,16 +8,19 @@ import redgreen.dawn.affectedfiles.model.AffectedFile.New
 import redgreen.dawn.affectedfiles.view.model.AffectedFileCellViewModel.FileCell
 
 /**
- * Comparators are usually implemented to sort file in ascending order. This one on the other hand sorts files in
- * a mix. It uses ascending and descending order based on certain criteria.
+ * Comparators are usually implemented to sort objects in ascending order. This one, on the other hand uses the
+ * following criteria to sort affected file cells.
  *
- * 1. Sort file cells based on the following change type order - New, Modified, Moved, and Deleted
- * 2. Sort file cells of similar change type based on change count in descending order.
+ * 1. Sort file cells based on the following change type order - New, Modified, Moved, and Deleted.
+ * 2. Sort file cells of similar change type based on change count in descending order (largest change count first).
  * 3. If file cells of the same type have similar change count, sort them in descending order based on deletions.
- * 4. If file cells of the same type don't have deletions, sort them in descending order based on insertions.
- * 5. If file cells of the same type have similar deletions, sort them in ascending order based on file names.
+ * 4. If there is a tie when using criterion 3, then sort them in ascending order based on filenames.
+ * 5. If file cells of the same type don't have deletions, sort them in descending order based on insertions.
  */
 object FileCellComparator : Comparator<FileCell> {
+  private const val ZERO_BECAUSE_A_NEW_FILE_CANT_HAVE_DELETIONS = 0
+  private const val BOTH_ARE_SAME = 0
+
   private val affectedFileRanks = mapOf(
     New::class to 1,
     Modified::class to 2,
@@ -32,35 +35,40 @@ object FileCellComparator : Comparator<FileCell> {
     val affectedFile1 = fileCell1.affectedFile
     val affectedFile2 = fileCell2.affectedFile
 
-    val rankComparisonResult = affectedFileRanks[affectedFile1::class]!! - affectedFileRanks[affectedFile2::class]!!
-    return if (rankComparisonResult != 0) {
-      rankComparisonResult
+    val rankComparisonResult = getRank(affectedFile1) - getRank(affectedFile2)
+    val differentRanks = rankComparisonResult != BOTH_ARE_SAME
+    if (differentRanks) {
+      return rankComparisonResult
+    }
+
+    val changeCountComparisonResult = affectedFile2.changeCount - affectedFile1.changeCount
+    val differentChangeCount = changeCountComparisonResult != BOTH_ARE_SAME
+    if (differentChangeCount) {
+      return changeCountComparisonResult
+    }
+
+    val deletionsComparisonResult = getDeletions(affectedFile2) - getDeletions(affectedFile1)
+    val differentDeletionsCount = deletionsComparisonResult != BOTH_ARE_SAME
+    return if (differentDeletionsCount) {
+      deletionsComparisonResult
     } else {
-      val changeCountComparisonResult = affectedFile2.changeCount - affectedFile1.changeCount
-      if (changeCountComparisonResult != 0) {
-        changeCountComparisonResult
-      } else {
-        val deletionsComparisonResult = getDeletions(affectedFile2) - getDeletions(affectedFile1)
-        if (deletionsComparisonResult != 0) {
-          deletionsComparisonResult
-        } else {
-          compareFileNames(affectedFile1.filename, affectedFile2.filename)
-        }
-      }
+      compareFileNames(affectedFile1.filename, affectedFile2.filename)
     }
   }
+
+  private fun getRank(affectedFile: AffectedFile): Int =
+    affectedFileRanks[affectedFile::class]!!
 
   private fun compareFileNames(
     filename1: String,
     filename2: String
-  ): Int {
-    return filename1.compareTo(filename2)
-  }
+  ): Int =
+    filename1.compareTo(filename2)
 
   private fun getDeletions(
     affectedFile: AffectedFile
   ): Int = when (affectedFile) {
-    is New -> 0
+    is New -> ZERO_BECAUSE_A_NEW_FILE_CANT_HAVE_DELETIONS
     is Modified -> affectedFile.deletions
     is Moved -> affectedFile.deletions
     is Deleted -> affectedFile.deletions
