931e8b253d38fe875697eee0f2a9af83a4675f97
Ragunath Jawahar <ragunath@redgreen.io>
2021-03-09 19:33:37 +0530

    build: remove the application Gradle plugin from :visuals-demo

--- visuals-demo/build.gradle.kts
+++ visuals-demo/build.gradle.kts
@@ -1,5 +1,4 @@
 plugins {
-  application
   kotlin("jvm")
   id("org.openjfx.javafxplugin") version "0.0.9"
 }
@@ -8,8 +7,6 @@ javafx {
   version = "15"
   modules("javafx.controls", "javafx.web")
 }
-
-application.mainClassName = "io.redgreen.visuals.demo.VisualsDemoApp"
 
 dependencies {
   implementation(project(":visuals"))
