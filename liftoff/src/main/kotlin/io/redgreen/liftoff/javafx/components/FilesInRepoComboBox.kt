82288b9a20c13e2cd756052f62bf30d61630f1f2
Ragunath Jawahar <ragunath@redgreen.io>
2021-03-14 10:54:29 +0530

    chore: fix unused private member detekt issue

--- liftoff/src/main/kotlin/io/redgreen/liftoff/javafx/components/FilesInRepoComboBox.kt
+++ liftoff/src/main/kotlin/io/redgreen/liftoff/javafx/components/FilesInRepoComboBox.kt
@@ -1,9 +1,6 @@
 package io.redgreen.liftoff.javafx.components
 
 import org.controlsfx.control.SearchableComboBox
-import org.eclipse.jgit.lib.Constants.HEAD
-import org.eclipse.jgit.lib.Repository
-import org.eclipse.jgit.treewalk.TreeWalk
 
 class FilesInRepoComboBox(onSelected: (filePath: String) -> Unit) : SearchableComboBox<String>() {
   init {
@@ -13,18 +10,5 @@ class FilesInRepoComboBox(onSelected: (filePath: String) -> Unit) : SearchableCo
         onSelected(newPath)
       }
     }
-  }
-
-  private fun Repository.getFilePaths(): List<String> {
-    val headCommit = this.parseCommit(this.resolve(HEAD))
-    val filePaths = mutableListOf<String>()
-    TreeWalk(this).use { treeWalk ->
-      treeWalk.reset(headCommit.tree.id)
-      treeWalk.isRecursive = true
-      while (treeWalk.next()) {
-        filePaths.add(treeWalk.pathString)
-      }
-    }
-    return filePaths.toList()
   }
 }
