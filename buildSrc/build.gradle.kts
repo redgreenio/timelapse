7cadbdd059367a06ac9fb9c754726aed831e2d5a
Ragunath Jawahar <ragunath@redgreen.io>
2021-03-31 20:36:40 +0530

    build: bump up Kotlin

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -7,13 +7,13 @@ repositories {
 }
 
 plugins {
-  kotlin("jvm") version "1.4.31"
+  kotlin("jvm") version "1.4.32"
   `java-gradle-plugin`
   `kotlin-dsl`
 }
 
 dependencies {
-  implementation(kotlin("gradle-plugin", "1.4.31"))
+  implementation(kotlin("gradle-plugin", "1.4.32"))
   implementation("com.github.ben-manes:gradle-versions-plugin:0.38.0")
   implementation("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.16.0")
   implementation("org.openjfx:javafx-plugin:0.0.9")
