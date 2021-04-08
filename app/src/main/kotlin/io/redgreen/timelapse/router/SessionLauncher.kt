80e0097aeb49f145b3393a39b7fe1bf0cd3e5cdf
Ragunath Jawahar <ragunath@redgreen.io>
2021-04-08 15:20:02 +0530

    refactor: simplify `SessionLauncher` logic

--- app/src/main/kotlin/io/redgreen/timelapse/router/SessionLauncher.kt
+++ app/src/main/kotlin/io/redgreen/timelapse/router/SessionLauncher.kt
@@ -2,7 +2,7 @@ package io.redgreen.timelapse.router
 
 import io.redgreen.timelapse.git.model.GitDirectory
 import io.redgreen.timelapse.openrepo.storage.RecentGitRepositoriesStorage
-import io.redgreen.timelapse.router.Destination.WELCOME_SCREEN
+import io.redgreen.timelapse.router.Destination.WORKBENCH
 
 class SessionLauncher(
   private val recentGitRepositoriesStorage: RecentGitRepositoriesStorage,
@@ -12,20 +12,23 @@ class SessionLauncher(
     launchWorkbenchAction: (gitRepositoryPath: String) -> Unit,
     launchWelcomeScreenAction: () -> Unit
   ) {
-    if (recentGitRepositoriesStorage.getSessionExitDestination() == WELCOME_SCREEN) {
-      launchWelcomeScreenAction()
-      return
-    }
-
-    val lastOpenedRepositoryPath = recentGitRepositoriesStorage
-      .getLastOpenedRepository()
-      .get()
-      .path
-
-    if (checkIfIsGitRepo(lastOpenedRepositoryPath)) {
-      launchWorkbenchAction(lastOpenedRepositoryPath)
+    if (canLaunchWorkbench(lastGitRepositoryPath())) {
+      launchWorkbenchAction(lastGitRepositoryPath()!!)
     } else {
       launchWelcomeScreenAction()
     }
+  }
+
+  private fun canLaunchWorkbench(lastGitRepositoryPath: String?): Boolean {
+    return recentGitRepositoriesStorage.getSessionExitDestination() == WORKBENCH &&
+      lastGitRepositoryPath != null &&
+      checkIfIsGitRepo(lastGitRepositoryPath)
+  }
+
+  private fun lastGitRepositoryPath(): String? {
+    return recentGitRepositoriesStorage
+      .getLastOpenedRepository()
+      .orElseGet { null }
+      ?.path
   }
 }
