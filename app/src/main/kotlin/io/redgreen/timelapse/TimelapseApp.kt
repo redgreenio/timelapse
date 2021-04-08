081f7ba8c4686fb8e42814a6d874434b00ff950c
Ragunath Jawahar <ragunath@redgreen.io>
2021-04-08 13:15:53 +0530

    feat: launch previously opened repo after exiting from the workbench

--- app/src/main/kotlin/io/redgreen/timelapse/TimelapseApp.kt
+++ app/src/main/kotlin/io/redgreen/timelapse/TimelapseApp.kt
@@ -4,9 +4,11 @@ import io.redgreen.timelapse.foo.exitOnClose
 import io.redgreen.timelapse.main.TimelapseScene
 import io.redgreen.timelapse.openrepo.storage.PreferencesRecentGitRepositoriesStorage
 import io.redgreen.timelapse.openrepo.view.OpenRepoScene
-import io.redgreen.timelapse.router.Destination.WELCOME_SCREEN
+import io.redgreen.timelapse.router.Destination.WORKBENCH
+import io.redgreen.timelapse.router.SessionLauncher
 import io.sentry.Sentry
 import javafx.application.Application
+import javafx.scene.Scene
 import javafx.stage.Stage
 
 private const val ALPHA = "\uD835\uDEFC"
@@ -27,21 +29,34 @@ class TimelapseApp : Application() {
   }
 
   override fun start(primaryStage: Stage) {
-    primaryStage.exitOnClose {
-      PreferencesRecentGitRepositoriesStorage().setSessionExitDestination(WELCOME_SCREEN)
-    }
-
-    val repositoryPath = if (parameters.raw.size != 0) parameters.raw.first() else null
-    val shouldLaunchTimelapseScene = repositoryPath != null
-    val sceneToShow = if (shouldLaunchTimelapseScene && repositoryPath != null) {
-      TimelapseScene(repositoryPath)
+    val preferencesRecentGitRepositoriesStorage = PreferencesRecentGitRepositoriesStorage()
+    val destination = preferencesRecentGitRepositoriesStorage.getSessionExitDestination()
+    if (destination == WORKBENCH) {
+      SessionLauncher(preferencesRecentGitRepositoriesStorage).tryRestorePreviousSession(
+        { launchWorkbench(primaryStage, it) },
+        { launchWelcomeScreen(primaryStage) }
+      )
     } else {
-      OpenRepoScene()
+      launchWelcomeScreen(primaryStage)
     }
 
-    with(primaryStage) {
+    primaryStage.exitOnClose {
+      preferencesRecentGitRepositoriesStorage.setSessionExitDestination(destination)
+    }
+  }
+
+  private fun launchWorkbench(stage: Stage, gitRepositoryPath: String) {
+    launchScene(stage, TimelapseScene(gitRepositoryPath), true)
+  }
+
+  private fun launchWelcomeScreen(stage: Stage) {
+    launchScene(stage, OpenRepoScene(), false)
+  }
+
+  private fun launchScene(stage: Stage, sceneToShow: Scene, resizable: Boolean) {
+    with(stage) {
       scene = sceneToShow
-      isResizable = shouldLaunchTimelapseScene
+      isResizable = resizable
       title = APP_NAME
       centerOnScreen()
       show()
