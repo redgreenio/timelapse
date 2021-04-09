037e6d21afc7240db85398dcf00c386104b1400b
Varsha <varsha.aux@redgreen.io>
2021-04-09 13:06:54 +0530

    feat: show menu for recently opened repositories

--- app/src/main/kotlin/io/redgreen/timelapse/workbench/menu/OpenRecentMenuViewModel.kt
+++ app/src/main/kotlin/io/redgreen/timelapse/workbench/menu/OpenRecentMenuViewModel.kt
@@ -2,4 +2,8 @@ package io.redgreen.timelapse.workbench.menu
 
 sealed class OpenRecentMenuViewModel {
   object EmptyMenu : OpenRecentMenuViewModel()
+  data class NonEmptyMenu(val contents: List<Any>) : OpenRecentMenuViewModel() {
+    data class RecentRepositoryMenuItemViewModel(val gitProjectDirectory: String)
+    object ClearRecentsMenuItemViewModel
+  }
 }
