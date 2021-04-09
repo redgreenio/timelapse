94e85d6b1753456ec46d1ceadc14d0ffadf53d73
Varsha <varsha.aux@redgreen.io>
2021-04-09 13:06:54 +0530

    feat: show menu for recently opened repositories

--- app/src/main/kotlin/io/redgreen/timelapse/workbench/menu/OpenRecentMenuViewModelUseCase.kt
+++ app/src/main/kotlin/io/redgreen/timelapse/workbench/menu/OpenRecentMenuViewModelUseCase.kt
@@ -2,12 +2,19 @@ package io.redgreen.timelapse.workbench.menu
 
 import io.redgreen.timelapse.openrepo.storage.RecentGitRepositoriesStorage
 import io.redgreen.timelapse.workbench.menu.OpenRecentMenuViewModel.EmptyMenu
+import io.redgreen.timelapse.workbench.menu.OpenRecentMenuViewModel.NonEmptyMenu
 
 class OpenRecentMenuViewModelUseCase(
   private val repositoriesStorage: RecentGitRepositoriesStorage
 ) {
   fun invoke(): OpenRecentMenuViewModel {
-    println(repositoriesStorage)
-    return EmptyMenu
+    val recentRepositories = repositoriesStorage.getRecentRepositories()
+    return if (recentRepositories.isEmpty()) {
+      EmptyMenu
+    } else {
+      val menuItemViewModels = recentRepositories
+        .map { NonEmptyMenu.RecentRepositoryMenuItemViewModel(it.path) }
+      NonEmptyMenu(menuItemViewModels + NonEmptyMenu.ClearRecentsMenuItemViewModel)
+    }
   }
 }
--- app/src/test/kotlin/io/redgreen/timelapse/workbench/menu/OpenRecentMenuViewModelUseCaseTest.kt
+++ app/src/test/kotlin/io/redgreen/timelapse/workbench/menu/OpenRecentMenuViewModelUseCaseTest.kt
@@ -3,19 +3,23 @@ package io.redgreen.timelapse.workbench.menu
 import com.google.common.truth.Truth.assertThat
 import com.nhaarman.mockitokotlin2.mock
 import com.nhaarman.mockitokotlin2.whenever
+import io.redgreen.timelapse.openrepo.data.RecentGitRepository
 import io.redgreen.timelapse.openrepo.storage.RecentGitRepositoriesStorage
 import io.redgreen.timelapse.workbench.menu.OpenRecentMenuViewModel.EmptyMenu
+import io.redgreen.timelapse.workbench.menu.OpenRecentMenuViewModel.NonEmptyMenu
+import io.redgreen.timelapse.workbench.menu.OpenRecentMenuViewModel.NonEmptyMenu.ClearRecentsMenuItemViewModel
+import io.redgreen.timelapse.workbench.menu.OpenRecentMenuViewModel.NonEmptyMenu.RecentRepositoryMenuItemViewModel
 import org.junit.jupiter.api.Test
 
 class OpenRecentMenuViewModelUseCaseTest {
+  private val repositoriesStorage = mock<RecentGitRepositoriesStorage>()
+  private val useCase = OpenRecentMenuViewModelUseCase(repositoriesStorage)
+
   @Test
   fun `it should return an empty view model when there are no recent repositories`() {
     // given
-    val repositoriesStorage = mock<RecentGitRepositoriesStorage>()
     whenever(repositoriesStorage.getRecentRepositories())
       .thenReturn(emptyList())
-
-    val useCase = OpenRecentMenuViewModelUseCase(repositoriesStorage)
 
     // when
     val openRecentMenuViewModel = useCase.invoke()
@@ -23,5 +27,29 @@ class OpenRecentMenuViewModelUseCaseTest {
     // then
     assertThat(openRecentMenuViewModel)
       .isEqualTo(EmptyMenu)
+  }
+
+  @Test
+  fun `it should return recent repositories menu items and clear recent menu items if there are recent repositories`() {
+    // given
+    val recentRepositories = listOf("/Projects/shopping-app/.git", "/Projects/coffee/.git")
+      .map(::RecentGitRepository)
+    whenever(repositoriesStorage.getRecentRepositories())
+      .thenReturn(recentRepositories)
+
+    // when
+    val openRecentMenuViewModel = useCase.invoke()
+
+    // then
+    assertThat(openRecentMenuViewModel)
+      .isInstanceOf(NonEmptyMenu::class.java)
+
+    assertThat((openRecentMenuViewModel as NonEmptyMenu).contents)
+      .containsExactly(
+        RecentRepositoryMenuItemViewModel("/Projects/shopping-app/.git"),
+        RecentRepositoryMenuItemViewModel("/Projects/coffee/.git"),
+        ClearRecentsMenuItemViewModel
+      )
+      .inOrder()
   }
 }
