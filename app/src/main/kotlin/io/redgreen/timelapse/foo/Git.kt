5c0fbc05247bc5f93c24edbd45f99efb42e66cc8
Ragunath Jawahar <ragunath@redgreen.io>
2021-04-09 17:22:00 +0530

    refactor: eliminate git repository check duplication

--- app/src/main/kotlin/io/redgreen/timelapse/foo/Git.kt
+++ app/src/main/kotlin/io/redgreen/timelapse/foo/Git.kt
@@ -1,8 +1,11 @@
 package io.redgreen.timelapse.foo
 
+import io.redgreen.timelapse.git.model.GitDirectory
 import java.io.File
 
 private const val GIT_DIRECTORY_NAME = ".git"
+
+val IS_GIT_REPOSITORY_PREDICATE: (path: String) -> Boolean = { GitDirectory.from(it).isPresent }
 
 fun String.appendDotGit(): String {
   return if (this.endsWith(File.separatorChar)) {
