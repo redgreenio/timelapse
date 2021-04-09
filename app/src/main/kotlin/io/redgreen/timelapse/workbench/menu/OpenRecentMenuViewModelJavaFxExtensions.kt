88f3e0530d6e06cc3bc2814ed1ed6952db532358
Varsha <varsha.aux@redgreen.io>
2021-04-09 15:33:22 +0530

    feat: show recent repositories menu items

--- app/src/main/kotlin/io/redgreen/timelapse/workbench/menu/OpenRecentMenuViewModelJavaFxExtensions.kt
+++ app/src/main/kotlin/io/redgreen/timelapse/workbench/menu/OpenRecentMenuViewModelJavaFxExtensions.kt
@@ -1,13 +1,34 @@
 package io.redgreen.timelapse.workbench.menu
 
 import io.redgreen.timelapse.workbench.menu.OpenRecentMenuViewModel.Empty
+import io.redgreen.timelapse.workbench.menu.OpenRecentMenuViewModel.NonEmpty
 import javafx.scene.control.Menu
+import javafx.scene.control.MenuItem
 
 private const val MENU_FILE_MENU_OPEN_RECENT = "Open Recent"
 
 @Suppress("unused") // Because, the type `Empty` itself provides us enough information.
-fun Empty.toJavaFxMenu() : Menu {
+fun Empty.toJavaFxMenu(): Menu {
   return Menu(MENU_FILE_MENU_OPEN_RECENT).apply {
     isDisable = true
+  }
+}
+
+fun NonEmpty.toJavaFxMenu(): Menu {
+  val menuItems = menuItemViewModels.map {
+    when (it) {
+      OpenRecentMenuItemViewModel.ClearRecent -> MenuItem("Clear Recent")
+      is OpenRecentMenuItemViewModel.RecentRepository -> MenuItem(it.repositoryDirectory)
+    }
+  }
+  return Menu(MENU_FILE_MENU_OPEN_RECENT).apply {
+    this.items.addAll(menuItems)
+  }
+}
+
+fun OpenRecentMenuViewModel.toJavaFxMenu(): Menu {
+  return when (this) {
+    Empty -> (this as Empty).toJavaFxMenu()
+    is NonEmpty -> this.toJavaFxMenu()
   }
 }
