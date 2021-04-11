ee4a62a0647ea20ed5114f33f8bc32a78ab0963c
Ragunath Jawahar <ragunath@redgreen.io>
2021-04-11 09:00:25 +0530

    refactor: extract open recent repository logic to its own class

---
+++ app/src/main/kotlin/io/redgreen/timelapse/workbench/menu/javafx/OpenRecentRepositoryActionEvent.kt
@@ -0,0 +1,18 @@
+package io.redgreen.timelapse.workbench.menu.javafx
+
+import io.redgreen.timelapse.foo.closeWindow
+import io.redgreen.timelapse.main.TimelapseScene
+import javafx.event.ActionEvent
+import javafx.event.EventHandler
+import javafx.scene.Scene
+import javafx.stage.Stage
+
+class OpenRecentRepositoryActionEvent(
+  private val scene: Scene,
+  private val repositoryDirectoryPath: String
+) : EventHandler<ActionEvent> {
+  override fun handle(event: ActionEvent) {
+    scene.closeWindow()
+    TimelapseScene.launch(Stage(), repositoryDirectoryPath)
+  }
+}
