df8014021a3ffcabae30ca4d48868b5bcfb14322
Ragunath Jawahar <ragunath@redgreen.io>
2021-04-11 09:00:25 +0530

    refactor: extract open recent repository logic to its own class

--- app/src/main/kotlin/io/redgreen/timelapse/workbench/menu/javafx/OpenRecentMenuViewModelJavaFxExtensions.kt
+++ app/src/main/kotlin/io/redgreen/timelapse/workbench/menu/javafx/OpenRecentMenuViewModelJavaFxExtensions.kt
@@ -1,7 +1,5 @@
 package io.redgreen.timelapse.workbench.menu.javafx
 
-import io.redgreen.timelapse.foo.closeWindow
-import io.redgreen.timelapse.main.TimelapseScene
 import io.redgreen.timelapse.workbench.menu.OpenRecentMenuItemViewModel
 import io.redgreen.timelapse.workbench.menu.OpenRecentMenuItemViewModel.ClearRecent
 import io.redgreen.timelapse.workbench.menu.OpenRecentMenuItemViewModel.RecentRepository
@@ -13,7 +11,6 @@ import javafx.scene.Scene
 import javafx.scene.control.Menu
 import javafx.scene.control.MenuItem
 import javafx.scene.control.SeparatorMenuItem
-import javafx.stage.Stage
 
 private const val MENU_FILE_MENU_OPEN_RECENT = "Open Recent"
 private const val MENU_OPEN_MENU_ITEM_CLEAR_RECENT = "Clear Recent"
@@ -44,10 +41,7 @@ private fun toMenuItem(
   ClearRecent -> MenuItem(MENU_OPEN_MENU_ITEM_CLEAR_RECENT)
   is RecentRepository -> MenuItem(menuItemViewModel.repositoryDirectory).apply {
     isDisable = !menuItemViewModel.isPresent
-    setOnAction {
-      scene.closeWindow()
-      TimelapseScene.launch(Stage(), menuItemViewModel.repositoryDirectory)
-    }
+    onAction = OpenRecentRepositoryActionEvent(scene, menuItemViewModel.repositoryDirectory)
   }
   Separator -> SeparatorMenuItem()
 }
