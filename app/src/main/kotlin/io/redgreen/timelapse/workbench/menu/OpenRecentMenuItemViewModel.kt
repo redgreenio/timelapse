28cd750a3685c936ccc256b4636722ad2a58ca99
Ragunath Jawahar <ragunath@redgreen.io>
2021-04-12 15:38:53 +0530

    refactor: introduce default null parameter

--- app/src/main/kotlin/io/redgreen/timelapse/workbench/menu/OpenRecentMenuItemViewModel.kt
+++ app/src/main/kotlin/io/redgreen/timelapse/workbench/menu/OpenRecentMenuItemViewModel.kt
@@ -1,10 +1,13 @@
 package io.redgreen.timelapse.workbench.menu
 
+import io.redgreen.timelapse.openrepo.data.RecentGitRepository
+
 sealed class OpenRecentMenuItemViewModel
 
 data class RecentRepositoryMenuItemViewModel(
   val repositoryDirectory: String,
-  val isPresent: Boolean = true
+  val isPresent: Boolean = true,
+  val recentRepository: RecentGitRepository? = null,
 ) : OpenRecentMenuItemViewModel()
 
 object ClearRecentMenuItemViewModel : OpenRecentMenuItemViewModel()
