ab0857a9d1e964ce97bdc7d76c820776e27782e5
Ragunath Jawahar <ragunath@redgreen.io>
2021-04-12 20:16:04 +0530

    refactor: move class to a different file

---
+++ app/src/main/kotlin/io/redgreen/timelapse/workbench/menu/DefaultFileMenuItemsClickListener.kt
@@ -0,0 +1,22 @@
+package io.redgreen.timelapse.workbench.menu
+
+import io.redgreen.timelapse.foo.closeWindow
+import io.redgreen.timelapse.main.TimelapseScene
+import io.redgreen.timelapse.openrepo.storage.PreferencesRecentGitRepositoriesStorage
+import javafx.scene.Scene
+import javafx.stage.Stage
+
+class DefaultFileMenuItemsClickListener(
+  private val scene: Scene,
+  private val openRepositoryPath: String
+) : FileMenuItemsClickListener {
+  override fun onRecentRepositoryClicked(directoryPath: String) {
+    scene.closeWindow()
+    TimelapseScene.launch(Stage(), directoryPath)
+  }
+
+  override fun onClearRecentClicked() {
+    PreferencesRecentGitRepositoriesStorage().clearRecentRepositories(openRepositoryPath)
+    WorkbenchMenu.install(scene, openRepositoryPath, true, this)
+  }
+}
--- app/src/main/kotlin/io/redgreen/timelapse/workbench/menu/FileMenuItemsClickListener.kt
+++ app/src/main/kotlin/io/redgreen/timelapse/workbench/menu/FileMenuItemsClickListener.kt
@@ -1,27 +1,6 @@
 package io.redgreen.timelapse.workbench.menu
 
-import io.redgreen.timelapse.foo.closeWindow
-import io.redgreen.timelapse.main.TimelapseScene
-import io.redgreen.timelapse.openrepo.storage.PreferencesRecentGitRepositoriesStorage
-import javafx.scene.Scene
-import javafx.stage.Stage
-
 interface FileMenuItemsClickListener {
   fun onClearRecentClicked()
   fun onRecentRepositoryClicked(directoryPath: String)
-}
-
-class DefaultFileMenuItemsClickListener(
-  private val scene: Scene,
-  private val openRepositoryPath: String
-) : FileMenuItemsClickListener {
-  override fun onRecentRepositoryClicked(directoryPath: String) {
-    scene.closeWindow()
-    TimelapseScene.launch(Stage(), directoryPath)
-  }
-
-  override fun onClearRecentClicked() {
-    PreferencesRecentGitRepositoriesStorage().clearRecentRepositories(openRepositoryPath)
-    WorkbenchMenu.install(scene, openRepositoryPath, true, this)
-  }
 }
