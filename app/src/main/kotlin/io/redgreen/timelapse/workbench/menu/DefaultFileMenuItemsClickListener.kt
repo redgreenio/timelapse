14886c7b3a461ca8913d2853bad51b517905dc1e
Ragunath Jawahar <ragunath@redgreen.io>
2021-04-12 20:19:20 +0530

    refactor: add callback function for "Close Project"

--- app/src/main/kotlin/io/redgreen/timelapse/workbench/menu/DefaultFileMenuItemsClickListener.kt
+++ app/src/main/kotlin/io/redgreen/timelapse/workbench/menu/DefaultFileMenuItemsClickListener.kt
@@ -3,6 +3,7 @@ package io.redgreen.timelapse.workbench.menu
 import io.redgreen.timelapse.foo.closeWindow
 import io.redgreen.timelapse.main.TimelapseScene
 import io.redgreen.timelapse.openrepo.storage.PreferencesRecentGitRepositoriesStorage
+import io.redgreen.timelapse.openrepo.view.OpenRepoScene
 import javafx.scene.Scene
 import javafx.stage.Stage
 
@@ -18,5 +19,10 @@ class DefaultFileMenuItemsClickListener(
   override fun onClearRecentClicked() {
     PreferencesRecentGitRepositoriesStorage().clearRecentRepositories(openRepositoryPath)
     WorkbenchMenu.install(scene, openRepositoryPath, true, this)
+  }
+
+  override fun onCloseProjectClicked() {
+    scene.closeWindow()
+    OpenRepoScene.launch(Stage())
   }
 }
