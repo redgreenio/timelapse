01202aea1e6b6fc9f567aaf5a979ebaeeec5c504
Ragunath Jawahar <ragunath@redgreen.io>
2021-04-12 20:10:17 +0530

    refactor: convert anonymous listener class to a named class

--- app/src/main/kotlin/io/redgreen/timelapse/workbench/menu/OpenRecentMenuItemsClickListener.kt
+++ app/src/main/kotlin/io/redgreen/timelapse/workbench/menu/OpenRecentMenuItemsClickListener.kt
@@ -1,6 +1,27 @@
 package io.redgreen.timelapse.workbench.menu
 
+import io.redgreen.timelapse.foo.closeWindow
+import io.redgreen.timelapse.main.TimelapseScene
+import io.redgreen.timelapse.openrepo.storage.PreferencesRecentGitRepositoriesStorage
+import javafx.scene.Scene
+import javafx.stage.Stage
+
 interface OpenRecentMenuItemsClickListener {
   fun onClearRecentClicked()
   fun onRecentRepositoryClicked(directoryPath: String)
+}
+
+class DefaultOpenRecentMenuItemsClickListener(
+  private val scene: Scene,
+  private val openRepositoryPath: String
+) : OpenRecentMenuItemsClickListener {
+  override fun onRecentRepositoryClicked(directoryPath: String) {
+    scene.closeWindow()
+    TimelapseScene.launch(Stage(), directoryPath)
+  }
+
+  override fun onClearRecentClicked() {
+    PreferencesRecentGitRepositoriesStorage().clearRecentRepositories(openRepositoryPath)
+    WorkbenchMenu.install(scene, openRepositoryPath, true, this)
+  }
 }
