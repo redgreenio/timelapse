8d0ebd2ccf3583771672eda1675faa16639baa7a
Varsha <varsha.aux@redgreen.io>
2021-04-12 16:08:18 +0530

    refactor: move `Empty` as top-level object

--- app/src/main/kotlin/io/redgreen/timelapse/workbench/menu/OpenRecentMenuViewModel.kt
+++ app/src/main/kotlin/io/redgreen/timelapse/workbench/menu/OpenRecentMenuViewModel.kt
@@ -1,8 +1,8 @@
 package io.redgreen.timelapse.workbench.menu
 
-sealed class OpenRecentMenuViewModel {
-  object Empty : OpenRecentMenuViewModel()
-}
+sealed class OpenRecentMenuViewModel
+
+object Empty : OpenRecentMenuViewModel()
 
 data class NonEmpty(
   val menuItemViewModels: List<OpenRecentMenuItemViewModel>
