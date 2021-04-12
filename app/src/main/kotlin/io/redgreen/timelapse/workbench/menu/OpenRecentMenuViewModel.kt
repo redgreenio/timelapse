0dad26bd99dc90370a5b4fc585555bb016ea95da
Varsha <varsha.aux@redgreen.io>
2021-04-12 16:02:54 +0530

    refactor: move `NonEmpty` as top-level class

--- app/src/main/kotlin/io/redgreen/timelapse/workbench/menu/OpenRecentMenuViewModel.kt
+++ app/src/main/kotlin/io/redgreen/timelapse/workbench/menu/OpenRecentMenuViewModel.kt
@@ -2,8 +2,8 @@ package io.redgreen.timelapse.workbench.menu
 
 sealed class OpenRecentMenuViewModel {
   object Empty : OpenRecentMenuViewModel()
-
-  data class NonEmpty(
-    val menuItemViewModels: List<OpenRecentMenuItemViewModel>
-  ) : OpenRecentMenuViewModel()
 }
+
+data class NonEmpty(
+  val menuItemViewModels: List<OpenRecentMenuItemViewModel>
+) : OpenRecentMenuViewModel()
--- app/src/test/kotlin/io/redgreen/timelapse/workbench/menu/OpenRecentMenuViewModelUseCaseTest.kt
+++ app/src/test/kotlin/io/redgreen/timelapse/workbench/menu/OpenRecentMenuViewModelUseCaseTest.kt
@@ -6,7 +6,6 @@ import com.nhaarman.mockitokotlin2.whenever
 import io.redgreen.timelapse.openrepo.data.RecentGitRepository
 import io.redgreen.timelapse.openrepo.storage.RecentGitRepositoriesStorage
 import io.redgreen.timelapse.workbench.menu.OpenRecentMenuViewModel.Empty
-import io.redgreen.timelapse.workbench.menu.OpenRecentMenuViewModel.NonEmpty
 import org.junit.jupiter.api.Test
 
 class OpenRecentMenuViewModelUseCaseTest {
