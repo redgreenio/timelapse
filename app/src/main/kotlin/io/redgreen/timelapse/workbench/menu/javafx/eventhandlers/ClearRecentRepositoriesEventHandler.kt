7efb0456f56e53f502d5c628473846a2ce01c80d
Ragunath Jawahar <ragunath@redgreen.io>
2021-04-12 19:48:03 +0530

    refactor: introduce parameter

--- app/src/main/kotlin/io/redgreen/timelapse/workbench/menu/javafx/eventhandlers/ClearRecentRepositoriesEventHandler.kt
+++ app/src/main/kotlin/io/redgreen/timelapse/workbench/menu/javafx/eventhandlers/ClearRecentRepositoriesEventHandler.kt
@@ -1,6 +1,7 @@
 package io.redgreen.timelapse.workbench.menu.javafx.eventhandlers
 
 import io.redgreen.timelapse.openrepo.storage.PreferencesRecentGitRepositoriesStorage
+import io.redgreen.timelapse.workbench.menu.OpenRecentMenuItemsClickListener
 import io.redgreen.timelapse.workbench.menu.WorkbenchMenu
 import javafx.event.ActionEvent
 import javafx.event.EventHandler
@@ -12,6 +13,11 @@ class ClearRecentRepositoriesEventHandler(
 ) : EventHandler<ActionEvent> {
   override fun handle(event: ActionEvent) {
     PreferencesRecentGitRepositoriesStorage().clearRecentRepositories(currentGitRepositoryPath)
-    WorkbenchMenu.install(scene, currentGitRepositoryPath, true)
+    WorkbenchMenu.install(
+      scene,
+      currentGitRepositoryPath,
+      true,
+      object : OpenRecentMenuItemsClickListener {}
+    )
   }
 }
