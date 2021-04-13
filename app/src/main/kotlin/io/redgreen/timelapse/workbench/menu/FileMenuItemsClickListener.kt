ccd517b8f007a7b7dd209b36982640c3f017a044
Ragunath Jawahar <ragunath@redgreen.io>
2021-04-13 15:51:00 +0530

    feat: add a new click event function for "Open..."

--- app/src/main/kotlin/io/redgreen/timelapse/workbench/menu/FileMenuItemsClickListener.kt
+++ app/src/main/kotlin/io/redgreen/timelapse/workbench/menu/FileMenuItemsClickListener.kt
@@ -1,6 +1,7 @@
 package io.redgreen.timelapse.workbench.menu
 
 interface FileMenuItemsClickListener {
+  fun onOpenClicked()
   fun onRecentClicked(repositoryPath: String)
   fun onClearRecentClicked()
   fun onCloseProjectClicked()
