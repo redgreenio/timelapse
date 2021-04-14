d3d415171b8b9157bafea00873b85f61bb7127e5
Varsha <varsha.aux@redgreen.io>
2021-04-14 13:04:27 +0530

    build: get first version for the given year

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -21,6 +21,7 @@ dependencies {
 
   testImplementation(kotlin("test-junit5"))
   testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.0")
+  testImplementation("org.junit.jupiter:junit-jupiter-params:5.7.0")
   testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.0")
   testImplementation("com.google.truth:truth:1.1.2")
 }
