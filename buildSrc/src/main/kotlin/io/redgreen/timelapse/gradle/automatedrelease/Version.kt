0038726e760a42a9cc04a4ba5a94ee8710b4d737
Ragunath Jawahar <ragunath@redgreen.io>
2021-04-15 13:00:54 +0530

    r - move class to a different compilation unit

---
+++ buildSrc/src/main/kotlin/io/redgreen/timelapse/gradle/automatedrelease/Version.kt
@@ -0,0 +1,48 @@
+package io.redgreen.timelapse.gradle.automatedrelease
+
+class Version(
+  val displayText: String,
+  private val yyyy: Int
+) {
+  private val versionComponents = "${displayText}..".split(".")
+  private val year = versionComponents[0]
+  private val publishedArtifactCount = versionComponents[1]
+  private val buildNumber = versionComponents[2]
+
+  fun nextVersion(isNextReleasePublic: Boolean): Version {
+    val releaseYear = getYear()
+    val nextPublishedArtifactCount = nextPublishedArtifactCount(isNextReleasePublic)
+    val nextBuildNumber = nextBuildNumber(isNextReleasePublic)
+    return Version("$releaseYear.$nextPublishedArtifactCount$nextBuildNumber", releaseYear.toInt())
+  }
+
+  private fun getYear(): String {
+    return if (displayText.isEmpty()) {
+      "$yyyy"
+    } else {
+      year
+    }
+  }
+
+  private fun nextPublishedArtifactCount(isNextReleasePublic: Boolean): Int {
+    return if (publishedArtifactCount.isEmpty() && isNextReleasePublic) {
+      1
+    } else if (isNextReleasePublic) {
+      publishedArtifactCount.toInt() + 1
+    } else if (publishedArtifactCount.isEmpty()) {
+      0
+    } else {
+      publishedArtifactCount.toInt()
+    }
+  }
+
+  private fun nextBuildNumber(isNextReleasePublic: Boolean): String {
+    return if (isNextReleasePublic) {
+      ""
+    } else if (buildNumber.isEmpty()) {
+      ".1"
+    } else {
+      ".${buildNumber.toInt() + 1}"
+    }
+  }
+}
