cb6d31066c7a23130575c4a9bdf67a089474af52
Ragunath Jawahar <ragunath@redgreen.io>
2021-04-15 11:44:50 +0530

    r - use build number in conditional 2

--- buildSrc/src/main/kotlin/io/redgreen/timelapse/gradle/automatedrelease/Versioning.kt
+++ buildSrc/src/main/kotlin/io/redgreen/timelapse/gradle/automatedrelease/Versioning.kt
@@ -23,7 +23,7 @@ object Versioning {
     return if (internalToInternal || internalOrPublicToPublic || internalOrPublicToInternal || isNextReleasePublic) {
       "$yearToUse.${getPublishedArtifactCount(isNextReleasePublic, publishedArtifactCount)}${getBuildNumber(isNextReleasePublic, buildNumber)}"
     } else {
-      "$yearToUse.0.1"
+      "$yearToUse.0${getBuildNumber(isNextReleasePublic, buildNumber)}"
     }
   }
 
