fdb67732d91a8b6f2a312312f57a62ab36f5470c
Ragunath Jawahar <ragunath@redgreen.io>
2021-04-17 09:06:46 +0530

    feat: print colored output for public and internal versions

--- buildSrc/src/main/kotlin/io/redgreen/timelapse/gradle/AutomatedReleasePlugin.kt
+++ buildSrc/src/main/kotlin/io/redgreen/timelapse/gradle/AutomatedReleasePlugin.kt
@@ -3,20 +3,45 @@ package io.redgreen.timelapse.gradle
 import io.redgreen.timelapse.gradle.automatedrelease.Versioning
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.internal.logging.text.StyledTextOutput
+import org.gradle.internal.logging.text.StyledTextOutputFactory
+import org.gradle.kotlin.dsl.support.serviceOf
 
 class AutomatedReleasePlugin : Plugin<Project> {
   override fun apply(target: Project) {
+    val output = target.serviceOf<StyledTextOutputFactory>().create("version-output")
+
     target.tasks.register("nextPublicVersion") {
-      description = "Prints the version text for the next public release."
+      val isPublic = true
+      val versionQualifier = "PUBLIC"
+      description = "Prints the version text for the next ${versionQualifier.toLowerCase()} release."
+      val nextVersion = Versioning.getNextVersion(getLatestVersion(), isPublic)
+      val message = "Next $versionQualifier version should be: "
+
       doLast {
-        println("Next PUBLIC version should be: ${Versioning.getNextVersion(getLatestVersion(), true)}")
+        output
+          .style(StyledTextOutput.Style.Success)
+          .text(message)
+          .style(StyledTextOutput.Style.SuccessHeader)
+          .text(nextVersion)
+          .println()
       }
     }
 
     target.tasks.register("nextInternalVersion") {
-      description = "Prints the version text for the next internal release."
+      val isPublic = false
+      val versionQualifier = "INTERNAL"
+      description = "Prints the version text for the next ${versionQualifier.toLowerCase()} release."
+      val nextVersion = Versioning.getNextVersion(getLatestVersion(), isPublic)
+      val message = "Next $versionQualifier version should be: "
+
       doLast {
-        println("Next INTERNAL version should be: ${Versioning.getNextVersion(getLatestVersion(), false)}")
+        output
+          .style(StyledTextOutput.Style.Success)
+          .text(message)
+          .style(StyledTextOutput.Style.SuccessHeader)
+          .text(nextVersion)
+          .println()
       }
     }
   }
