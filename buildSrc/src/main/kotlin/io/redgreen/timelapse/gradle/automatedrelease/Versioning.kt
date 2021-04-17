03553e3a50c64098e51c848534afcd086f82bdcf
Ragunath Jawahar <ragunath@redgreen.io>
2021-04-17 08:05:46 +0530

    refactor: get rid of temporary fix

--- buildSrc/src/main/kotlin/io/redgreen/timelapse/gradle/automatedrelease/Versioning.kt
+++ buildSrc/src/main/kotlin/io/redgreen/timelapse/gradle/automatedrelease/Versioning.kt
@@ -2,18 +2,10 @@ package io.redgreen.timelapse.gradle.automatedrelease
 
 object Versioning {
   fun getNextVersion(
-    yyyy: Int,
     displayText: String,
     isPublic: Boolean = true
   ): String {
-    // Temporary fix for tests that are sending empty strings for no previous releases
-    val versionToUse = if (displayText.isEmpty()) {
-      "$yyyy"
-    } else {
-      displayText
-    }
-
-    val version = Version.from(versionToUse)
+    val version = Version.from(displayText)
     return if (isPublic) {
       version.publicRelease()
     } else {
