4dadf3ce27189daf51927d8358616290692faed6
Ragunath Jawahar <ragunath@redgreen.io>
2021-04-18 22:13:05 +0530

    build: read and update CHANGELOG logic (don't write to disk yet!)

--- buildSrc/src/main/kotlin/io/redgreen/timelapse/gradle/automatedrelease/tasks/PrintVersionTask.kt
+++ buildSrc/src/main/kotlin/io/redgreen/timelapse/gradle/automatedrelease/tasks/PrintVersionTask.kt
@@ -1,7 +1,9 @@
 package io.redgreen.timelapse.gradle.automatedrelease.tasks
 
 import io.redgreen.timelapse.gradle.automatedrelease.Versioning
+import java.io.File
 import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.TaskAction
 import org.gradle.internal.logging.text.StyledTextOutput
@@ -9,6 +11,12 @@ import org.gradle.internal.logging.text.StyledTextOutputFactory
 import org.gradle.kotlin.dsl.support.serviceOf
 
 abstract class PrintVersionTask(private val isPublic: Boolean) : DefaultTask() {
+  companion object {
+    private const val CHANGELOG = "CHANGELOG.md"
+
+    private const val PRINT_DEBUG_INFO = false
+  }
+
   @TaskAction
   fun perform() {
     val versionQualifier = getVersionQualifier(isPublic)
@@ -17,6 +25,12 @@ abstract class PrintVersionTask(private val isPublic: Boolean) : DefaultTask() {
 
     val output = project.serviceOf<StyledTextOutputFactory>().create("version-output")
     printVersion(output, message, nextVersion)
+
+    // Step 2 - Update CHANGELOG.md
+    val updatedChangeLog = getUpdatedChangelogText(nextVersion)
+    if (PRINT_DEBUG_INFO) {
+      output.println(updatedChangeLog)
+    }
   }
 
   private fun getVersionQualifier(isPublic: Boolean): String {
@@ -58,5 +72,22 @@ abstract class PrintVersionTask(private val isPublic: Boolean) : DefaultTask() {
       .inputStream
       .reader()
       .use { it.readText() }
+  }
+
+  private fun getUpdatedChangelogText(version: String): String {
+    return File(CHANGELOG)
+      .readText()
+      .replace(
+        """
+            |## [Unreleased]
+            |
+            """.trimMargin("|"),
+        """
+            |## [Unreleased]
+            |
+            |## [$version] - ${LocalDate.now().format(DateTimeFormatter.ISO_LOCAL_DATE)}
+            |
+            """.trimMargin("|")
+      )
   }
 }
