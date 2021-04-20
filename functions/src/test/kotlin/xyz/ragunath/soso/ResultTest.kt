97c004c9f2cc8570e7e141509207c1aa246999cd
Ragunath Jawahar <ragunathjawahar@users.noreply.github.com>
2021-04-20 09:12:42 +0530

    Revert "Rename test functions for consistency"
    This reverts commit e9d49b32e1e5f084f48a3819c8c2bb24495281ca.

--- functions/src/test/kotlin/xyz/ragunath/soso/ResultTest.kt
+++ functions/src/test/kotlin/xyz/ragunath/soso/ResultTest.kt
@@ -5,21 +5,21 @@ import org.junit.Test
 
 class ResultTest {
   @Test
-  fun `it returns a length of 0 when the result is empty`() {
-    val result = Result.EMPTY
+  fun `when no function as found, then return a length of zero`() {
+    val result = Result.with(0, 0, 0)
     assertThat(result.length)
       .isEqualTo(0)
   }
 
   @Test
-  fun `it returns a length of 1 for single line functions`() {
+  fun `when a single line function was found, then return a length of 1`() {
     val result = Result.with(1, 1, 5)
     assertThat(result.length)
       .isEqualTo(1)
   }
 
   @Test
-  fun `it returns the actual length for a function that spans across multiple lines`() {
+  fun `when a function spans across multiple lines, then return its actual length`() {
     val result = Result.with(11, 17, 2)
     assertThat(result.length)
       .isEqualTo(7)
@@ -27,27 +27,27 @@ class ResultTest {
 
   // TODO(rj) 12/Oct/19 - Assert error messages as well.
   @Test(expected = IllegalStateException::class)
-  fun `it throws an exception when start line is greater than the end line`() {
+  fun `when start line is greater than the end line, then throw an exception`() {
     Result.with(11, 8, 1)
   }
 
   @Test(expected = IllegalStateException::class)
-  fun `it throws an exception when depth is not equal to zero for invalid start and end lines`() {
+  fun `when a function is non-existent, then the depth must be zero`() {
     Result.with(0, 0, 1)
   }
 
   @Test(expected = IllegalStateException::class)
-  fun `it throws an exception when start line is negative`() {
+  fun `when start line is not positive, throw an exception`() {
     Result.with(-1, 0, 1)
   }
 
   @Test(expected = IllegalStateException::class)
-  fun `it throws an exception when end line is negative`() {
+  fun `when end line is not positive, throw an exception`() {
     Result.with(5, -4, 1)
   }
 
   @Test(expected = IllegalStateException::class)
-  fun `it throws an exception when depth is negative`() {
+  fun `when depth is not positive, throw an exception`() {
     Result.with(1, 5, -1)
   }
 }
