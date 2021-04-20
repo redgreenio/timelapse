6ec4ae18c8709eddac2cbde84c5e0663db1b5aef
Ragunath Jawahar <ragunathjawahar@users.noreply.github.com>
2021-04-20 09:12:42 +0530

    Revert "Replace string matching logic with character reading logic"
    This reverts commit e13ff252c4cad3a7a1fa8337c5a01f4263312fcb.

--- functions/src/main/kotlin/xyz/ragunath/soso/swift/SwiftScanner.kt
+++ functions/src/main/kotlin/xyz/ragunath/soso/swift/SwiftScanner.kt
@@ -1,50 +1,32 @@
 package xyz.ragunath.soso.swift
 
 import xyz.ragunath.soso.PossibleFunction
-import xyz.ragunath.soso.extensions.push
 
 private const val KEYWORD_FUNC = "func "
 
-private const val MODE_SEEK_FUNCTION = 0
-private const val MODE_SEEK_FUNCTION_NAME = 1
-private const val MODE_SEEK_OPEN_PARENTHESIS = 2
-
 fun swiftScan(code: String): List<PossibleFunction> {
   val possibleFunctions = mutableListOf<PossibleFunction>()
-  val buffer = CharArray(KEYWORD_FUNC.length)
-  val codeChars = code.toCharArray()
+  val lines = code.lines()
   var lineNumber = 1
-  var possibleFunctionLineNumber = lineNumber
-  var mode = MODE_SEEK_FUNCTION
-  val functionName = mutableListOf<Char>()
 
-  for (char in codeChars) {
-    buffer.push(char)
+  for (line in lines) {
+    if (line.contains(KEYWORD_FUNC)) {
+      val startIndex = line.indexOf(KEYWORD_FUNC)
+      when {
+        line.trim().startsWith("//") -> println("Found commented line `$line` on line $lineNumber. PTAL?")
 
-    if (mode == MODE_SEEK_FUNCTION) {
-      if (String(buffer) == KEYWORD_FUNC) { // FIXME, this will cause too many allocations and GC
-        possibleFunctionLineNumber = lineNumber
-        mode = MODE_SEEK_FUNCTION_NAME
+        line.trim().contains('{') -> {
+          val functionName = line.substring(startIndex + KEYWORD_FUNC.length, line.indexOf("(", startIndex))
+          possibleFunctions.add(PossibleFunction(functionName, lineNumber))
+        }
+
+        else -> {
+          val functionName = line.substring(startIndex + KEYWORD_FUNC.length, line.indexOf("(", startIndex))
+          println("Found `$functionName` on line $lineNumber. Excluding from result, PTAL?")
+        }
       }
     }
-
-    if (char != ' ' && char != '(' && mode == MODE_SEEK_FUNCTION_NAME) {
-      functionName.add(char)
-    } else if (char == '(') {
-      mode = MODE_SEEK_OPEN_PARENTHESIS
-    }
-
-    if (char == '{' && mode == MODE_SEEK_OPEN_PARENTHESIS) {
-      possibleFunctions.add(
-        PossibleFunction(functionName.joinToString(""), possibleFunctionLineNumber)
-      )
-      mode = MODE_SEEK_FUNCTION
-      functionName.clear()
-    }
-
-    if (char == '\n') {
-      lineNumber++
-    }
+    lineNumber++
   }
 
   return possibleFunctions.toList()
