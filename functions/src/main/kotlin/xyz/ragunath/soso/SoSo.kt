48313a8ecdcf0b296cb9d5b94a31516d1f2225a0
Ragunath Jawahar <ragunathjawahar@users.noreply.github.com>
2021-04-20 09:12:42 +0530

    Revert "Rename function and extract variables for better test readability"
    This reverts commit 9fdabc24c4187fb29278f1700b12db5adc6744e0.

--- functions/src/main/kotlin/xyz/ragunath/soso/SoSo.kt
+++ functions/src/main/kotlin/xyz/ragunath/soso/SoSo.kt
@@ -3,7 +3,7 @@ package xyz.ragunath.soso
 typealias Depth = Int
 
 object SoSo {
-  fun depthOf(code: String): Depth {
+  fun maximumDepthOf(code: String): Depth {
     return code
       .fold(0) { depth, char ->
         if (char == '{') depth + 1 else depth
--- functions/src/test/kotlin/xyz/ragunath/soso/SoSoTest.kt
+++ functions/src/test/kotlin/xyz/ragunath/soso/SoSoTest.kt
@@ -6,44 +6,47 @@ import org.junit.Test
 class SoSoTest {
   @Test
   fun `when there are no brackets, then return a depth of 0`() {
-    val noBrackets = ""
-    assertThat(SoSo.depthOf(noBrackets))
+    val depth = SoSo.maximumDepthOf("")
+    assertThat(depth)
       .isEqualTo(0)
   }
 
   @Test
   fun `when there is a pair of brackets, then return a depth of 1 (single line)`() {
-    val onePairOfBracketsSingleLine = "{}"
-    assertThat(SoSo.depthOf(onePairOfBracketsSingleLine))
+    val depth = SoSo.maximumDepthOf("{}")
+    assertThat(depth)
       .isEqualTo(1)
   }
 
   @Test
   fun `when there are two pairs of brackets, then return a depth of 2 (single line)`() {
-    val twoPairsOfBracketsSingleLine = "{{}}"
-    assertThat(SoSo.depthOf(twoPairsOfBracketsSingleLine))
+    val depth = SoSo.maximumDepthOf("{{}}")
+    assertThat(depth)
       .isEqualTo(2)
   }
 
   @Test
   fun `when there is a pair of brackets on different lines, then return a depth of 1`() {
-    val onePairOfBracketDifferentLines = """
-        {
-        }
-      """.trimIndent()
+    val depth = SoSo.maximumDepthOf(
+      """
+      {
+      }
+    """.trimIndent()
+    )
 
-    assertThat(SoSo.depthOf(onePairOfBracketDifferentLines))
+    assertThat(depth)
       .isEqualTo(1)
   }
 
   @Test
   fun `when there is a Kotlin function, then report a depth of 1`() {
-    val mainFunction = """
+    val depth = SoSo.maximumDepthOf(
+      """
       fun main() {
       }
     """.trimIndent()
-
-    assertThat(SoSo.depthOf(mainFunction))
+    )
+    assertThat(depth)
       .isEqualTo(1)
   }
 
@@ -57,7 +60,7 @@ class SoSoTest {
       }
     """.trimIndent()
 
-    assertThat(SoSo.depthOf(functionWithConditional))
+    assertThat(SoSo.maximumDepthOf(functionWithConditional))
       .isEqualTo(2)
   }
 }
