b167442782031288883d17d5b71623fd9f397aa9
Ragunath Jawahar <ragunathjawahar@users.noreply.github.com>
2021-04-20 09:12:42 +0530

    Revert "Run parser on malformed Swift validation tests source"
    This reverts commit acc43e9e12a1a89a53a2868534754effbcbf045a.

--- functions/src/main/kotlin/xyz/ragunath/childsplay/ChildsPlay.kt
+++ functions/src/main/kotlin/xyz/ragunath/childsplay/ChildsPlay.kt
@@ -24,6 +24,7 @@ fun main() {
     .asSequence()
     .filter { it.fileName.toString().endsWith(".swift") }
     .filter { !it.toAbsolutePath().toString().contains("/Pods/") }
+    .filter { !it.toAbsolutePath().toString().contains("/validation-test/") }
     .map {
       println("Reading contents from $it")
       Files.readAllBytes(it)
