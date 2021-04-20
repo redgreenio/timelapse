975ce867537b4874138b6704a543fa4b218dab5d
Ragunath Jawahar <ragunathjawahar@users.noreply.github.com>
2021-04-20 09:12:42 +0530

    Revert "style: fix indentation"
    This reverts commit 2c09d211bb510f9cdab1f3e1dc381b0b8c98510b.

--- functions/src/test/kotlin/io/redgreen/scout/ParseTest.kt
+++ functions/src/test/kotlin/io/redgreen/scout/ParseTest.kt
@@ -55,8 +55,7 @@ class ParseTest {
 
   @Test
   fun `it can analyze matching brackets on two lines`() {
-    val onePairOfBracketDifferentLines =
-      """
+    val onePairOfBracketDifferentLines = """
         {
         }
       """.trimIndent()
@@ -286,7 +285,7 @@ class ParseTest {
   @Test
   fun `it can ignore contents of multiline string literals`() {
     val functionWithMultilineStringLiteral =
-      "func multilineStrings() {\n" +
+        "func multilineStrings() {\n" +
         "  println(\"\"\" { Inside a multiline string literal } \"\"\")\n" +
         "}"
 
