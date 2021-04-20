da07b8b02a2dc2a294a09cdca1ed9efd58d9a482
Ragunath Jawahar <ragunathjawahar@users.noreply.github.com>
2021-04-20 09:12:42 +0530

    Revert "Get rid of extension function"
    This reverts commit b89e4381e866b29afe0e74a8e73fb01020bd6960.

--- functions/src/main/kotlin/xyz/ragunath/soso/Result.kt
+++ functions/src/main/kotlin/xyz/ragunath/soso/Result.kt
@@ -9,10 +9,11 @@ data class Result(
     val EMPTY = with(0, 0, 0)
 
     fun with(startLine: Int, endLine: Int, depth: Int): Result {
-      check(startLine >= 0) { "`startLine`: $startLine should be a positive integer" }
-      check(endLine >= 0) { "`endLine`: $endLine should be a positive integer" }
-      check(depth >= 0) { "`depth`: $depth should be a positive integer" }
-      check(!(startLine == 0 && endLine == 0 && depth != 0)) { "`depth` must be zero for a non-existent function, but was `$depth`" }
+      check(startLine.isPositive) { "`startLine`: $startLine should be a positive integer" }
+      check(endLine.isPositive) { "`endLine`: $endLine should be a positive integer" }
+      check(depth.isPositive) { "`depth`: $depth should be a positive integer" }
+      val nonExistentFunction = startLine == 0 && endLine == 0
+      check(!(nonExistentFunction && depth != 0)) { "`depth` must be zero for a non-existent function, but was `$depth`" }
       check(startLine <= endLine) { "`startLine`: $startLine cannot be greater than `endLine`: $endLine" }
       return Result(startLine, endLine, depth)
     }
@@ -28,3 +29,6 @@ data class Result(
   fun withOffset(offset: Int): Result =
     copy(startLine = offset + startLine, endLine = offset + endLine)
 }
+
+private val Int.isPositive: Boolean
+  get() = this >= 0
