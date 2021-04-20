c9b14ba1fbf939794dce177b39f1b2f8f8d08e27
Ragunath Jawahar <ragunath@redgreen.io>
2021-04-20 16:56:57 +0530

    feat: detect deleted functions

--- app/src/test/kotlin/io/redgreen/timelapse/extendeddiff/CompareTest.kt
+++ app/src/test/kotlin/io/redgreen/timelapse/extendeddiff/CompareTest.kt
@@ -3,6 +3,8 @@ package io.redgreen.timelapse.extendeddiff
 import com.google.common.truth.Truth.assertThat
 import io.redgreen.scout.ParseResult
 import io.redgreen.scout.languages.kotlin.KotlinFunctionScanner
+import io.redgreen.timelapse.extendeddiff.ComparisonResult.Added
+import io.redgreen.timelapse.extendeddiff.ComparisonResult.Deleted
 import org.junit.jupiter.api.Test
 
 class CompareTest {
@@ -28,7 +30,7 @@ class CompareTest {
     // then
     assertThat(affectedFunctions)
       .containsExactly(
-        ComparisonResult.Added(ParseResult.wellFormedFunction("b", 4, 5, 1))
+        Added(ParseResult.wellFormedFunction("b", 4, 5, 1))
       )
   }
 
@@ -57,8 +59,35 @@ class CompareTest {
     // then
     assertThat(affectedFunctions)
       .containsExactly(
-        ComparisonResult.Added(ParseResult.wellFormedFunction("b", 4, 5, 1)),
-        ComparisonResult.Added(ParseResult.wellFormedFunction("c", 7, 8, 1))
+        Added(ParseResult.wellFormedFunction("b", 4, 5, 1)),
+        Added(ParseResult.wellFormedFunction("c", 7, 8, 1))
+      )
+      .inOrder()
+  }
+
+  @Test
+  fun `it should detect a deleted function`() {
+    // given
+    val before = """
+      fun a() {
+      }
+
+      fun b() {
+      }
+    """.trimIndent()
+
+    val after = """
+      fun a() {
+      }
+    """.trimIndent()
+
+    // when
+    val affectedFunctions = compare(before, after, KotlinFunctionScanner)
+
+    // then
+    assertThat(affectedFunctions)
+      .containsExactly(
+        Deleted(ParseResult.wellFormedFunction("b", 4, 5, 1))
       )
   }
 }
