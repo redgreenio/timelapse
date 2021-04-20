73f165ea10e9f8f39b484bb9ad3376c8b3229895
Ragunath Jawahar <ragunathjawahar@users.noreply.github.com>
2021-04-20 09:12:42 +0530

    Revert "Rename a lot of variables"
    This reverts commit 7fa240675606d91dea50827e4b0afed48414bf29.

--- functions/src/main/kotlin/xyz/ragunath/soso/FunctionAnalyzer.kt
+++ functions/src/main/kotlin/xyz/ragunath/soso/FunctionAnalyzer.kt
@@ -9,7 +9,7 @@ typealias Length = Int
 private const val TOKEN_OPEN_CURLY = '{'
 private const val TOKEN_CLOSE_CURLY = '}'
 private const val TOKEN_FORWARD_SLASH = '/'
-private const val TOKEN_NEW_LINE = '\n'
+private const val TOKEN_NEW_LINE = '\n' // TODO, this should be passed from outside
 private const val TOKEN_ASTERISK = '*'
 
 private const val SCAN_DEPTH = 1
@@ -18,23 +18,23 @@ private const val SKIP_MULTILINE_COMMENT = 3
 
 fun analyze(snippet: String): List<Result> {
   val results = mutableListOf<Result>()
-  val snippetChars = snippet.toCharArray()
-  var lastKnownChar = NULL_CHAR
+  val chars = snippet.toCharArray()
+  var previousChar = NULL_CHAR
 
   var mode = SCAN_DEPTH
-  var maximumDepth = 0
-  var lineNumber = 1
-  var startLineNumber = 0
-  var endLineNumber: Int
+  var maximumDepthCount = 0
+  var lineCount = 1
+  var startLine = 0
+  var endLine: Int
   val depthStack = Stack<Depth>()
-  for (char in snippetChars) {
+  for (char in chars) {
     if (char == TOKEN_NEW_LINE) {
-      lineNumber++
+      lineCount++
     }
 
     when {
-      isSingleLineComment(lastKnownChar, char) -> mode = SKIP_SINGLE_LINE_COMMENT
-      isMultilineComment(mode, lastKnownChar, char) -> mode = SKIP_MULTILINE_COMMENT
+      isSingleLineComment(previousChar, char) -> mode = SKIP_SINGLE_LINE_COMMENT
+      isMultilineComment(mode, previousChar, char) -> mode = SKIP_MULTILINE_COMMENT
     }
 
     when (mode) {
@@ -42,43 +42,38 @@ fun analyze(snippet: String): List<Result> {
         mode = SCAN_DEPTH
       }
 
-      SKIP_MULTILINE_COMMENT -> if (lastKnownChar == TOKEN_ASTERISK && char == TOKEN_FORWARD_SLASH) {
+      SKIP_MULTILINE_COMMENT -> if (previousChar == TOKEN_ASTERISK && char == TOKEN_FORWARD_SLASH) {
         mode = SCAN_DEPTH
       }
 
       SCAN_DEPTH -> when (char) {
         TOKEN_OPEN_CURLY -> {
           if (depthStack.isEmpty()) {
-            maximumDepth = 1
-            startLineNumber = lineNumber
-            depthStack.push(maximumDepth)
+            maximumDepthCount = 1
+            startLine = lineCount
+            depthStack.push(maximumDepthCount)
           } else {
-            val depth = depthStack.peek() + 1
-            if (depth > maximumDepth) {
-              maximumDepth = depth
+            val depthCount = depthStack.peek() + 1
+            if (depthCount > maximumDepthCount) {
+              maximumDepthCount = depthCount
             }
-            depthStack.push(depth)
+            depthStack.push(depthCount)
           }
         }
 
         TOKEN_CLOSE_CURLY -> {
           depthStack.pop()
-
           if (depthStack.isEmpty()) {
-            endLineNumber = lineNumber
-            val result = Result.with(
-              maximumDepth,
-              calculateFunctionLength(startLineNumber, endLineNumber, maximumDepth),
-              startLineNumber,
-              endLineNumber
+            endLine = lineCount
+            results.add(
+              Result.with(maximumDepthCount, calculateFunctionLength(startLine, endLine, maximumDepthCount), startLine, endLine)
             )
-            results.add(result)
           }
         }
       }
     }
 
-    lastKnownChar = char
+    previousChar = char
   }
 
   return results.toList()
@@ -101,7 +96,7 @@ private fun calculateFunctionLength(
   startLine: Int,
   endLine: Int,
   depth: Int
-): Int = if (startLine == endLine && depth == 0) {
+): Int = if (endLine == startLine && depth == 0) {
   0
 } else {
   endLine - startLine + 1
