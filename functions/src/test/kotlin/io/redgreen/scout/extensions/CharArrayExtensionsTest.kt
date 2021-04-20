923564620069b106b58182681ccd92fb0344bc6b
Ragunath Jawahar <ragunathjawahar@users.noreply.github.com>
2021-04-20 09:12:42 +0530

    Revert "Refactor tests for `CharArrayExtensions`"
    This reverts commit 7b9b99eeab12bb3aa9cbcad35b8b04ebe6d60cc6.

--- functions/src/test/kotlin/io/redgreen/scout/extensions/CharArrayExtensionsTest.kt
+++ functions/src/test/kotlin/io/redgreen/scout/extensions/CharArrayExtensionsTest.kt
@@ -7,120 +7,104 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
 class CharArrayExtensionsTest {
-  @Nested
-  inner class Push {
-    private val size4CharArray = CharArray(4)
+  private val nullChar = '\u0000'
+  private val charArray = CharArray(4)
 
-    @Test
-    fun `it can push an element into an array`() {
-      val nullChar = '\u0000'
-      with(size4CharArray) {
-        // when
-        push('1')
+  @Test
+  fun `it can push elements into an array`() {
+    charArray.push('1')
+    assertThat(String(charArray))
+      .isEqualTo("$nullChar$nullChar${nullChar}1")
+  }
 
-        // then
-        assertContainsExactly(nullChar, nullChar, nullChar, '1')
-      }
+  @Test
+  fun `it can push n elements into an array`() {
+    with(charArray) {
+      push('1')
+      push('2')
+      push('3')
+      push('4')
     }
 
-    @Test
-    fun `it can push 'n' elements into an array of size 'n'`() {
-      with(size4CharArray) {
-        // when
-        push('1')
-        push('2')
-        push('3')
-        push('4')
+    assertThat(String(charArray))
+      .isEqualTo("1234")
+  }
 
-        // then
-        assertContainsExactly('1', '2', '3', '4')
-      }
+  @Test
+  fun `it can overwrite the old buffer when new chars are pushed into it`() {
+    with(charArray) {
+      push('1'); push('2'); push('3'); push('4')
+      push('5'); push('6')
     }
 
-    @Test
-    fun `it can overwrite old content when newer chars are pushed into the array`() {
-      with(size4CharArray) {
-        // when
-        push('1'); push('2'); push('3')
-        push('4'); push('5'); push('6')
+    assertThat(String(charArray))
+      .isEqualTo("3456")
+  }
 
-        // then
-        assertContainsExactly('3', '4', '5', '6')
-      }
+  @Test
+  fun `it throws an exception when chars are pushed into an array with length 0`() {
+    val exception = assertThrows<IllegalStateException> {
+      val zeroLengthArray = CharArray(0)
+      zeroLengthArray.push('a')
     }
+    assertThat(exception.message)
+      .isEqualTo("Cannot push into a zero-length array")
+  }
+
+  @Test
+  fun `it can push items into a char array of size 1`() {
+    val size1CharArray = CharArray(1)
+    size1CharArray.push('a')
+
+    assertThat(String(size1CharArray))
+      .isEqualTo("a")
+  }
+
+  @Test
+  fun `it can overwrite items in a char array of size 1`() {
+    val size1CharArray = CharArray(1)
+    with(size1CharArray) {
+      push('a')
+      push('b')
+    }
+
+    assertThat(String(size1CharArray))
+      .isEqualTo("b")
+  }
+
+  @Test
+  fun `it can push items into a char array of size 2`() {
+    val size2CharArray = CharArray(2)
+    with(size2CharArray) {
+      push('a')
+      push('c')
+    }
+
+    assertThat(String(size2CharArray))
+      .isEqualTo("ac")
+  }
+
+  @Test
+  fun `it can overwrite items in a char array of size 2`() {
+    val size2CharArray = CharArray(2)
+    with(size2CharArray) {
+      push('a')
+      push('b')
+      push('c')
+      push('d')
+    }
+
+    assertThat(String(size2CharArray))
+      .isEqualTo("cd")
   }
 
   @Nested
-  inner class Size {
-    @Test
-    fun `it throws an exception when chars are pushed into an empty array`() {
-      val exception = assertThrows<IllegalStateException> {
-        // when
-        CharArray(0).also { it.push('a') }
-      }
-
-      // then
-      assertThat(exception.message)
-        .isEqualTo("Cannot push into an empty array")
-    }
-
-    @Test
-    fun `it can push items into an array of size 1`() {
-      with(CharArray(1)) {
-        // when
-        push('a')
-
-        // then
-        assertContainsExactly('a')
-      }
-    }
-
-    @Test
-    fun `it can overwrite items in an array of size 1`() {
-      with(CharArray(1)) {
-        // when
-        push('a')
-        push('b')
-
-        // then
-        assertContainsExactly('b')
-      }
-    }
-
-    @Test
-    fun `it can push items into an array of size 2`() {
-      with(CharArray(2)) {
-        // when
-        push('a')
-        push('c')
-
-        // then
-        assertContainsExactly('a', 'c')
-      }
-    }
-
-    @Test
-    fun `it can overwrite items in an array of size 2`() {
-      with(CharArray(2)) {
-        // when
-        push('a')
-        push('b')
-        push('c')
-        push('d')
-
-        // then
-        assertContainsExactly('c', 'd')
-      }
-    }
-  }
-
-  @Nested
-  inner class EndsWith {
-    private val alphabetsCharArray = CharArray(4)
+  inner class EndsWithTest {
+    private val alphabets = CharArray(4)
 
     @BeforeEach
     fun setup() {
-      with(alphabetsCharArray) {
+      with(alphabets) {
         push('a')
         push('b')
         push('c')
@@ -130,43 +114,32 @@ class CharArrayExtensionsTest {
 
     @Test
     fun `it returns true if both array contents are equal`() {
-      val abcd = "abcd".toCharArray()
-      assertThat(alphabetsCharArray.endsWith(abcd))
+      assertThat(alphabets.endsWith("abcd".toCharArray()))
         .isTrue()
     }
 
     @Test
     fun `it returns true if the array ends with a subset of chars`() {
-      val bcd = "bcd".toCharArray()
-      assertThat(alphabetsCharArray.endsWith(bcd))
+      assertThat(alphabets.endsWith("bcd".toCharArray()))
         .isTrue()
     }
 
     @Test
     fun `it returns false if the contents don't match`() {
-      val numbers1234 = "1234".toCharArray()
-      assertThat(alphabetsCharArray.endsWith(numbers1234))
+      assertThat(alphabets.endsWith("1234".toCharArray()))
         .isFalse()
     }
 
     @Test
     fun `it returns false if the passed in array is larger than the given array`() {
-      val abcde = "abcde".toCharArray()
-      assertThat(alphabetsCharArray.endsWith(abcde))
+      assertThat(alphabets.endsWith("abcde".toCharArray()))
         .isFalse()
     }
 
     @Test
     fun `it returns false if the passed in array is empty`() {
-      val emptyArray = CharArray(0)
-      assertThat(alphabetsCharArray.endsWith(emptyArray))
+      assertThat(alphabets.endsWith(CharArray(0)))
         .isFalse()
     }
-  }
-
-  private fun CharArray.assertContainsExactly(vararg chars: Char) {
-    assertThat(this.toList())
-      .containsExactly(*chars.toTypedArray())
-      .inOrder()
   }
 }
