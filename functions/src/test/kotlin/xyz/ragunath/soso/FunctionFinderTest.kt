0e474c2f58a4beaa23e6f4f1b3ceed342860c470
Ragunath Jawahar <ragunathjawahar@users.noreply.github.com>
2021-04-20 09:12:42 +0530

    Revert "Get rid of old finder logic"
    This reverts commit 12445b3f33b1cd1503ad9805b9c48fb3dd9a92c9.

---
+++ functions/src/test/kotlin/xyz/ragunath/soso/FunctionFinderTest.kt
@@ -0,0 +1,37 @@
+package xyz.ragunath.soso
+
+import com.google.common.truth.Truth.assertThat
+import org.junit.Test
+
+// TODO Run an accuracy test for detection for each language
+class FunctionFinderTest {
+  @Test
+  fun `it can detect if there are no functions`() {
+    val blankText = "          "
+
+    assertThat(findFunction(blankText))
+      .isEqualTo(Location.NONE)
+  }
+
+  @Test
+  fun `it can return the start and end row of a function declaration`() {
+    val singleLineFunction = """fun main() { println("Ta da!") }"""
+
+    assertThat(findFunction(singleLineFunction))
+      .isEqualTo(Location(1, 1))
+  }
+
+  @Test
+  fun `it can return the start and end row of a function preceded by non-function lines`() {
+    val singleLineFunction = """
+      package xyz.ragunath.foo
+
+      // This file contains a main function.
+
+      fun main() { println("Bang bang!") }
+      """.trimIndent()
+
+    assertThat(findFunction(singleLineFunction))
+      .isEqualTo(Location(5, 5))
+  }
+}
