c8bcba18af0ec2d028e5d799e268153397002cb8
Ragunath Jawahar <ragunath@redgreen.io>
2021-04-20 12:14:57 +0530

    feat: move adding escaped newline chars to the `applyPatch` function

--- app/src/main/kotlin/io/redgreen/timelapse/extendeddiff/Patch.kt
+++ app/src/main/kotlin/io/redgreen/timelapse/extendeddiff/Patch.kt
@@ -3,10 +3,30 @@ package io.redgreen.timelapse.extendeddiff
 import ext.name.fraser.neil.plaintext.diff_match_patch
 import java.util.LinkedList
 
+/* Please refer: https://github.com/google/diff-match-patch/wiki/Unidiff */
+private const val ESCAPED_NEWLINE = "%0A"
+
+private const val INSERTION_CHAR = '+'
+private const val NEWLINE_CHAR = '\n'
+
 fun applyPatch(text: String, patch: String): String {
   val diffMatchPatch = diff_match_patch()
-  val patchFromText = diffMatchPatch.patch_fromText(patch)
+  val patchFromText = diffMatchPatch.patch_fromText(formatPatchForDiffMatchPatch(patch))
 
   val patchApply = diffMatchPatch.patch_apply(LinkedList(patchFromText), text)
   return patchApply.first().toString()
+}
+
+private fun formatPatchForDiffMatchPatch(patch: String): String {
+  val lines = patch.split(NEWLINE_CHAR)
+
+  return lines
+    .mapIndexed { index, line ->
+      if (line.startsWith(INSERTION_CHAR) && index != lines.lastIndex) {
+        "$line$ESCAPED_NEWLINE"
+      } else {
+        line
+      }
+    }
+    .joinToString(NEWLINE_CHAR.toString())
 }
