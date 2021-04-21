5ade71daea81fca42337582e5a63167a134d148e
Ragunath Jawahar <ragunath@redgreen.io>
2021-04-21 12:47:21 +0530

    feat: handle empty patches in the diff engine

--- app/src/main/kotlin/io/redgreen/timelapse/extendeddiff/ExtendedDiffEngine.kt
+++ app/src/main/kotlin/io/redgreen/timelapse/extendeddiff/ExtendedDiffEngine.kt
@@ -1,9 +1,16 @@
 package io.redgreen.timelapse.extendeddiff
 
+import io.redgreen.timelapse.extendeddiff.ExtendedDiff.NoChanges
+
 class ExtendedDiffEngine private constructor(private val seedText: String) {
   companion object {
     fun newInstance(seedText: String): ExtendedDiffEngine {
       return ExtendedDiffEngine(seedText)
     }
+  }
+
+  fun extendedDiff(patch: String): Any {
+    patch.length // To satisfy the linter
+    return NoChanges(seedText)
   }
 }
