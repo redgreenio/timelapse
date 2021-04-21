5737fc90563d93c0e0feb01d26a06c2512f3762a
Ragunath Jawahar <ragunath@redgreen.io>
2021-04-21 17:11:12 +0530

    feat: format modified function to HTML

--- app/src/test/kotlin/io/redgreen/timelapse/extendeddiff/ExtendedDiffHtmlTest.kt
+++ app/src/test/kotlin/io/redgreen/timelapse/extendeddiff/ExtendedDiffHtmlTest.kt
@@ -1,6 +1,8 @@
 package io.redgreen.timelapse.extendeddiff
 
 import io.redgreen.scout.ParseResult
+import io.redgreen.timelapse.extendeddiff.ComparisonResult.Added
+import io.redgreen.timelapse.extendeddiff.ComparisonResult.Modified
 import io.redgreen.timelapse.extendeddiff.ExtendedDiff.HasChanges
 import org.approvaltests.Approvals
 import org.junit.jupiter.api.Test
@@ -15,10 +17,31 @@ class ExtendedDiffHtmlTest {
     """.trimIndent()
 
     val comparisonResults = listOf(
-      ComparisonResult.Added(ParseResult.wellFormedFunction("b", 2, 2, 1))
+      Added(ParseResult.wellFormedFunction("b", 2, 2, 1))
     )
 
     val hasChanges = HasChanges(kotlinSource, comparisonResults)
+
+    // when & then
+    Approvals.verifyHtml(hasChanges.toHtml())
+  }
+
+  @Test
+  fun `it should handle modified function`() {
+    // given
+    val swiftSource = """
+      class SomeClass {
+        func a() {
+          // Hello, world!
+        }
+      }
+    """.trimIndent()
+
+    val comparisonResults = listOf(
+      Modified(ParseResult.wellFormedFunction("a", 2, 4, 1))
+    )
+
+    val hasChanges = HasChanges(swiftSource, comparisonResults)
 
     // when & then
     Approvals.verifyHtml(hasChanges.toHtml())
