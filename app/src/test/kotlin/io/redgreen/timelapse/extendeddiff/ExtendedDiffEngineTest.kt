7a433f6e1c393cffb36822db0679b01d874ebbdf
Varsha <varsha.aux@redgreen.io>
2021-04-21 13:42:00 +0530

    feat: apply successive patches using the diff engine

--- app/src/test/kotlin/io/redgreen/timelapse/extendeddiff/ExtendedDiffEngineTest.kt
+++ app/src/test/kotlin/io/redgreen/timelapse/extendeddiff/ExtendedDiffEngineTest.kt
@@ -3,6 +3,8 @@ package io.redgreen.timelapse.extendeddiff
 import com.google.common.truth.Truth.assertThat
 import io.redgreen.scout.ParseResult
 import io.redgreen.timelapse.extendeddiff.ComparisonResult.Added
+import io.redgreen.timelapse.extendeddiff.ComparisonResult.Deleted
+import io.redgreen.timelapse.extendeddiff.ComparisonResult.Modified
 import io.redgreen.timelapse.extendeddiff.ExtendedDiff.HasChanges
 import io.redgreen.timelapse.extendeddiff.ExtendedDiff.NoChanges
 import org.junit.jupiter.api.Test
@@ -58,6 +60,51 @@ class ExtendedDiffEngineTest {
     """.trimIndent()
     val comparisonResults = listOf(
       Added(ParseResult.wellFormedFunction("b", 2, 2, 1))
+    )
+
+    assertThat(extendedDiff)
+      .isEqualTo(HasChanges(patchedText, comparisonResults))
+  }
+
+  @Test
+  fun `it should apply successive patches and return an extended diff`() {
+    // given
+    val seedText = "fun a() {}"
+
+    val patch1 = """
+      --- a.txt	2021-04-21 12:49:53.000000000 +0530
+      +++ b.txt	2021-04-21 12:50:04.000000000 +0530
+      @@ -1 +1,2 @@
+      -fun a() {}
+      \ No newline at end of file
+      +fun a() {}
+      +fun b() {}
+      \ No newline at end of file
+    """.trimIndent()
+
+    val patch2 = """
+      --- a.txt	2021-04-21 13:25:55.000000000 +0530
+      +++ b.txt	2021-04-21 13:26:00.000000000 +0530
+      @@ -1,2 +1 @@
+      -fun a() {}
+       fun b() {}
+      \ No newline at end of file
+    """.trimIndent()
+
+    val patches = listOf(patch1, patch2)
+    val diffEngine = ExtendedDiffEngine.newInstance(seedText)
+
+    // when
+    diffEngine.extendedDiff(patch1)
+    val extendedDiff = diffEngine.extendedDiff(patch2)
+
+    // then
+    val patchedText = """
+      fun b() {}
+    """.trimIndent()
+    val comparisonResults = listOf(
+      Deleted(ParseResult.wellFormedFunction("a", 1, 1, 1)),
+      Modified(ParseResult.wellFormedFunction("b", 1, 1, 1))
     )
 
     assertThat(extendedDiff)
