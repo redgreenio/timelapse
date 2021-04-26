a82d5aa029a8dff39e8e253b8c3a70e3b457297a
Ragunath Jawahar <ragunath@redgreen.io>
2021-04-26 14:03:04 +0530

    feat: render seed source text on the screen

--- app/src/quickFeedback/kotlin/extendeddiff/ExtendedDiffController.kt
+++ app/src/quickFeedback/kotlin/extendeddiff/ExtendedDiffController.kt
@@ -1,5 +1,10 @@
 package extendeddiff
 
+import extendeddiff.samplecreator.Sample
+import extendeddiff.samplecreator.Samples
+import io.redgreen.scout.languages.kotlin.KotlinFunctionScanner
+import io.redgreen.timelapse.extendeddiff.ExtendedDiffEngine
+import io.redgreen.timelapse.extendeddiff.toHtml
 import javafx.fxml.FXML
 import javafx.scene.web.WebView
 
@@ -8,6 +13,23 @@ class ExtendedDiffController {
   private lateinit var codeViewerWebView: WebView
 
   fun start() {
-    codeViewerWebView.engine.loadContent("<b>Hello, world!</b>")
+    val sample = Samples.EXTENDED_DIFF
+    val seedSourceCode = readResourceFile("/samples/${sample.name}/seed.txt")
+    val diffEngine = ExtendedDiffEngine.newInstance(seedSourceCode, KotlinFunctionScanner)
+    val seedSourceCodeExtendedDiff = diffEngine.extendedDiff("")
+
+    val html = seedSourceCodeExtendedDiff.toHtml()
+    renderHtml(html)
+  }
+
+  private fun renderHtml(html: String) {
+    codeViewerWebView.engine.loadContent(html)
+  }
+
+  private fun readResourceFile(resourcePath: String): String {
+    return Sample::class.java
+      .getResourceAsStream(resourcePath)
+      .reader()
+      .readText()
   }
 }
