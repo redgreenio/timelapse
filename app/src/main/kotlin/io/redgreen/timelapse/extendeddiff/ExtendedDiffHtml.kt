99e22d228968558dab431328a29a3b3ab0ad791c
Varsha <varsha.aux@redgreen.io>
2021-04-27 11:13:48 +0530

    feat: display code in monospace font

--- app/src/main/kotlin/io/redgreen/timelapse/extendeddiff/ExtendedDiffHtml.kt
+++ app/src/main/kotlin/io/redgreen/timelapse/extendeddiff/ExtendedDiffHtml.kt
@@ -203,6 +203,10 @@ private fun htmlTemplate(tableRows: List<String>): String {
               .deleted {
                 background-color: $CSS_COLOR_DELETED;
               }
+              table td {
+                font-family: "monospace";
+                font-size: small;
+              }
             </style>
         </head>
         <body>
