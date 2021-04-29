8b599d85800c8c0e710dd03309b87a6b23bd1984
Ragunath Jawahar <ragunath@redgreen.io>
2021-04-29 17:08:14 +0530

    feat: add a real-world sample for extended diff

--- app/src/quickFeedback/kotlin/extendeddiff/samplecreator/Sample.kt
+++ app/src/quickFeedback/kotlin/extendeddiff/samplecreator/Sample.kt
@@ -2,9 +2,11 @@ package extendeddiff.samplecreator
 
 object Samples {
   internal val EXTENDED_DIFF = Sample("extended-diff", 36)
+  internal val SIMPLE_ANDROID = Sample("simple-android", 87, 29)
 }
 
 data class Sample(
   val name: String,
-  val patchesCount: Int
+  val patchesCount: Int,
+  val patchCountOffset: Int = 0
 )
