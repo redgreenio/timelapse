6303fb36b673f84b15d2692c77ccb3e6e7e7a076
Ragunath Jawahar <ragunath@redgreen.io>
2021-05-02 06:25:54 +0530

    feat: sample ANTLR grammar with a visitor

--- languages/build.gradle.kts
+++ languages/build.gradle.kts
@@ -1,9 +1,21 @@
+plugins {
+  id("antlr")
+}
+
 dependencies {
   implementation(deps.log4j.api)
   implementation(deps.log4j.core)
+
+  antlr("org.antlr:antlr4:4.9.2")
+  implementation("org.antlr:antlr4-runtime:4.9.2")
 
   testImplementation(deps.test.junit.api)
   testRuntimeOnly(deps.test.junit.engine)
 
   testImplementation(deps.test.truth)
+}
+
+tasks.generateGrammarSource {
+  maxHeapSize = "64m"
+  arguments = arguments + listOf("-visitor", "-long-messages")
 }
