fd535a349cb784c35a192eb5d18a9d1b7ce366b4
Ragunath Jawahar <ragunath@redgreen.io>
2021-05-03 18:19:11 +0530

    test: add canary test to `design` module

---
+++ design/src/test/kotlin/CanaryTest.kt
@@ -0,0 +1,10 @@
+import com.google.common.truth.Truth.assertThat
+import org.junit.jupiter.api.Test
+
+class CanaryTest {
+  @Test
+  fun `junit and truth are setup`() {
+    assertThat(true)
+      .isTrue()
+  }
+}
