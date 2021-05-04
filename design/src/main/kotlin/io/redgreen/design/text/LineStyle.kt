a87543344b594fc2497075791cdb849b03d8b5d4
Ragunath Jawahar <ragunath@redgreen.io>
2021-05-04 10:57:01 +0530

    feat: add support for multiple line styles

--- design/src/main/kotlin/io/redgreen/design/text/LineStyle.kt
+++ design/src/main/kotlin/io/redgreen/design/text/LineStyle.kt
@@ -3,4 +3,6 @@ package io.redgreen.design.text
 data class LineStyle(
   val name: String,
   val lineNumberRange: IntRange
-)
+) {
+  constructor(name: String, lineNumber: Int) : this(name, lineNumber..lineNumber)
+}
