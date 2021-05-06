4a4ddb25a784d2e59278780e5fa53dd08772084d
Ragunath Jawahar <ragunath@redgreen.io>
2021-05-06 16:26:39 +0530

    y

--- app/src/quickFeedback/java/Math.kt
+++ app/src/quickFeedback/java/Math.kt
@@ -1,12 +1,14 @@
-fun add(x: Int, y: Int): Int =
+import java.math.BigDecimal
+
+fun add(x: BigDecimal, y: BigDecimal): BigDecimal =
   x + y
 
 private fun bestMatchingMethod(
   clazz: Class<*>,
   methodName: String,
-  parameterTypes: Array<Class<out Any>>
+  parameterTypes: List<Class<out Any>>
 ) = try {
-  clazz.getDeclaredMethod(methodName, *parameterTypes)
+  clazz.getDeclaredMethod(methodName, *parameterTypes.toTypedArray())
 } catch (exception: NoSuchMethodException) {
   clazz.declaredMethods.first { it.name == methodName }
 }
