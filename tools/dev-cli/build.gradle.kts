80b99b22f4ca78a8270cf9d9c9720d079066f040
Ragunath Jawahar <ragunath@redgreen.io>
2021-05-08 18:20:21 +0530

    build: create an executable binary for the cli tool

--- tools/dev-cli/build.gradle.kts
+++ tools/dev-cli/build.gradle.kts
@@ -1,9 +1,12 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
 plugins {
   application
+  id("com.github.johnrengelman.shadow") version "7.0.0"
 }
 
 application {
-  mainClass.set("io.redgreen.timelapse.devcli.MainKt")
+  mainClassName = "io.redgreen.timelapse.devcli.MainKt"
 }
 
 dependencies {
@@ -17,4 +20,38 @@ dependencies {
   testRuntimeOnly(deps.test.junit.engine)
 
   testImplementation(deps.test.approvalTests)
+}
+
+tasks {
+  named<ShadowJar>("shadowJar") {
+    archiveBaseName.set("dev-cli")
+    mergeServiceFiles()
+
+    manifest {
+      attributes(mapOf("Main-Class" to "io.redgreen.timelapse.devcli.MainKt"))
+    }
+  }
+}
+
+tasks {
+  build {
+    dependsOn(shadowJar)
+  }
+}
+
+tasks.register("executable", DefaultTask::class) {
+  description = "Creates self-executable file, that runs generated shadow jar"
+  group = "Distribution"
+
+  inputs.files(tasks.named("shadowJar"))
+  outputs.file("${buildDir.resolve("exec").resolve("time")}")
+
+  doLast {
+    val execFile = outputs.files.singleFile
+
+    execFile.outputStream().use {
+      it.write("#!/bin/sh\n\nexec java -Xmx512m -jar \"\$0\" \"\$@\"\n\n".toByteArray())
+      it.write(inputs.files.singleFile.readBytes())
+    }
+  }
 }
