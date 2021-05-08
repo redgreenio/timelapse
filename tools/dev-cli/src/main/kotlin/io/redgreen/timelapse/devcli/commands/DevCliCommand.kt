0a3df60b35dc306cdaa26497c77d1eae8d08096f
Ragunath Jawahar <ragunath@redgreen.io>
2021-05-08 20:36:12 +0530

    build: add CLI version

--- tools/dev-cli/src/main/kotlin/io/redgreen/timelapse/devcli/commands/DevCliCommand.kt
+++ tools/dev-cli/src/main/kotlin/io/redgreen/timelapse/devcli/commands/DevCliCommand.kt
@@ -10,5 +10,6 @@ import picocli.CommandLine.Command
   subcommands = [CreateBaseHtmlSubcommand::class, CreateXdSampleSubcommand::class],
   description = ["Accelerator for developing Timelapse features."],
   commandListHeading = "%nCommands:%n%nAvailable commands are:%n",
+  version = ["v0.0.1"],
 )
 class DevCliCommand
