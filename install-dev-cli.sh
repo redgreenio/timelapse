5895532432120890b541f670038b263bc3130e22
Ragunath Jawahar <ragunath@redgreen.io>
2021-05-09 08:29:11 +0530

    build: run tests before creating a `dev-cli` executable

--- install-dev-cli.sh
+++ install-dev-cli.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 
-./gradlew tools:dev-cli:executable
+./gradlew tools:dev-cli:test tools:dev-cli:executable
 cp ./tools/dev-cli/build/exec/dev-cli /usr/local/bin/
 chmod +x /usr/local/bin/dev-cli
