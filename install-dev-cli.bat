791fc15cd3754fa09b6d05929821d80c4964c9de
Varsha <varsha.aux@redgreen.io>
2021-05-10 10:54:48 +0530

    chore: add `dev-cli` script for Windows

---
+++ install-dev-cli.bat
@@ -0,0 +1,12 @@
+@echo off
+Rem gradlew tools:dev-cli:test tools:dev-cli:shadowJar
+
+set devCliDirectory=%homedrive%%homepath%\.dev-cli
+echo %devCliDirectory%
+if not exist %devCliDirectory% mkdir %devCliDirectory%
+copy %cd%\tools\dev-cli\build\libs\dev-cli-all.jar %devCliDirectory%\dev-cli-all.jar
+(
+  echo @echo off
+  echo java -jar %devCliDirectory%\dev-cli-all.jar %%^*
+) > %devCliDirectory%\dev-cli.bat
+echo 'dev-cli' installed. Please add '%devCliDirectory%' to your 'Path' variable.
