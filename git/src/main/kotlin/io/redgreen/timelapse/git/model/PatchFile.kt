ae377acc6123fe0bfb774694164c71b156832da5
Ragunath Jawahar <ragunath@redgreen.io>
2021-05-17 05:05:43 +0530

    feat: add test to get affected multiple line numbers for side B

--- git/src/main/kotlin/io/redgreen/timelapse/git/model/PatchFile.kt
+++ git/src/main/kotlin/io/redgreen/timelapse/git/model/PatchFile.kt
@@ -2,11 +2,35 @@ package io.redgreen.timelapse.git.model
 
 class PatchFile(private val unifiedPatch: String) {
   companion object {
+    private const val CHAR_NEWLINE = "\n"
+    private const val CHAR_SPACE = " "
+
+    private const val INDEX_UNIFIED_DIFF_HEADER = 2
+    private const val INDEX_HUNK_B = 2
+
     fun from(unifiedPatch: String): PatchFile =
       PatchFile(unifiedPatch)
   }
 
   fun getAffectedLineNumbers(): List<Int> {
-    return listOf(1)
+    val lines = unifiedPatch.split(CHAR_NEWLINE)
+    val unifiedDiffHeader = lines[INDEX_UNIFIED_DIFF_HEADER]
+    val headerParts = unifiedDiffHeader.split(CHAR_SPACE)
+    val hunkHeaderB = HunkHeader.from(headerParts[INDEX_HUNK_B])
+    return listOf(hunkHeaderB.startLine, hunkHeaderB.extendsTill)
+      .distinct()
+  }
+}
+
+class HunkHeader(val startLine: Int, val extendsTill: Int) {
+  companion object {
+    private const val COMMA = ","
+
+    fun from(hunkHeaderText: String): HunkHeader {
+      val hunkHeaderTextSplit = hunkHeaderText.split(COMMA)
+      val startLine = hunkHeaderTextSplit.first().toString().drop(1).toInt()
+      val extendsTill = hunkHeaderTextSplit.last().toString().toInt()
+      return HunkHeader(startLine, extendsTill)
+    }
   }
 }
--- git/src/test/kotlin/io/redgreen/timelapse/git/model/PatchFileTest.kt
+++ git/src/test/kotlin/io/redgreen/timelapse/git/model/PatchFileTest.kt
@@ -23,4 +23,27 @@ internal class PatchFileTest {
     assertThat(affectedLineNumbers)
       .containsExactly(1)
   }
+
+  @Test
+  fun `it should get affected line numbers for side B (non-empty A, starting from 1)`() {
+    // given
+    val unifiedPatch = """
+      --- a.txt	2021-05-17 05:00:42.000000000 +0530
+      +++ b.txt	2021-05-17 05:00:56.000000000 +0530
+      @@ -1 +1,2 @@
+      -Hello, world!
+      \ No newline at end of file
+      +Hello, world!
+      +Enjoy enjaami!
+      \ No newline at end of file
+    """.trimIndent()
+    val patchFile = PatchFile.from(unifiedPatch)
+
+    // when
+    val affectedLineNumbers = patchFile.getAffectedLineNumbers()
+
+    // then
+    assertThat(affectedLineNumbers)
+      .containsExactly(1, 2)
+  }
 }
