62d63a0afefadb2af881d12b2d317621066dff34
Ragunath Jawahar <ragunath@redgreen.io>
2021-05-17 05:43:27 +0530

    feat: get affected line numbers for B (line numbers not starting at 1)

--- git/src/test/kotlin/io/redgreen/timelapse/git/model/PatchFileTest.kt
+++ git/src/test/kotlin/io/redgreen/timelapse/git/model/PatchFileTest.kt
@@ -46,4 +46,30 @@ internal class PatchFileTest {
     assertThat(affectedLineNumbers)
       .containsExactly(1, 2)
   }
+
+  @Test
+  fun `it should get affected line numbers for side B (non-empty A, not starting from 1)`() {
+    // given
+    val unifiedPatch = """
+      --- a.txt	2021-05-17 05:27:03.000000000 +0530
+      +++ b.txt	2021-05-17 05:27:15.000000000 +0530
+      @@ -1,3 +1,5 @@
+      Two roads diverged in a yellow wood,
+      And sorry I could not travel both
+      -And be one traveler, long I stood
+      \ No newline at end of file
+      +And be one traveler, long I stood
+      +And looked down one as far as I could
+      +To where it bent in the undergrowth;
+      \ No newline at end of file
+    """.trimIndent()
+    val patchFile = PatchFile.from(unifiedPatch)
+
+    // when
+    val affectedLineNumbers = patchFile.getAffectedLineNumbers()
+
+    // then
+    assertThat(affectedLineNumbers)
+      .containsExactly(3, 4, 5)
+  }
 }
