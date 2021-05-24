f5dc8ca5ff5d5e72b8ba903f31e0f9ae36681bb8
Ragunath Jawahar <ragunath@redgreen.io>
2021-05-24 17:05:34 +0530

    test: return an empty list of a non-existent identifier

--- xd-js/test/xd.test.js
+++ xd-js/test/xd.test.js
@@ -1,11 +1,11 @@
 const chai = require('chai');
 const should = chai.should;
-should()
+should();
 
 const {JSDOM} = require("jsdom");
 const { selectOccurrences } = require("../src/xd");
 
-const fs = require('fs')
+const fs = require('fs');
 
 describe("selectOccurrences", () => {
   const addFunctionHtml = fs.readFileSync("./test/assets/add-function.html");
@@ -13,17 +13,25 @@ describe("selectOccurrences", () => {
 
   it("should select all elements with identifier 'a'", () => {
     // when
-    const occurrences = selectOccurrences(window, "a")
+    const occurrences = selectOccurrences(window, "a");
 
     // then
-    occurrences.should.have.length(2)
-  })
+    occurrences.should.have.length(2);
+  });
 
   it("should select all elements with identifier 'b'", () => {
     // when
-    const occurrences = selectOccurrences(window, "b")
+    const occurrences = selectOccurrences(window, "b");
 
     // then
-    occurrences.should.have.length(2)
-  })
+    occurrences.should.have.length(2);
+  });
+
+  it("should return an empty list if the identifier does not exist", () => {
+    // when
+    const occurrences = selectOccurrences(window, "z");
+
+    // then
+    occurrences.should.have.length(0);
+  });
 });
