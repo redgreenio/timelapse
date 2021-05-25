62dee4a492bb6181835f9e931f45d284dd522ba7
Ragunath Jawahar <ragunath@redgreen.io>
2021-05-25 09:23:19 +0530

    style: adhere to airbnb JS guide

--- xd-js/test/canary.test.js
+++ xd-js/test/canary.test.js
@@ -1,15 +1,16 @@
-const assert = require("assert");
+const assert = require('assert');
 
-const {JSDOM} = require("jsdom");
-const {window} = new JSDOM('<!DOCTYPE html><p>Hello world</p>');
-const $ = require("jquery")(window);
+const { JSDOM } = require('jsdom');
 
-describe("a test environment", () => {
-  it("should have mocha setup", () => {
+const { window } = new JSDOM('<!DOCTYPE html><p>Hello world</p>');
+const $ = require('jquery')(window);
+
+describe('a test environment', () => {
+  it('should have mocha setup', () => {
     assert.strictEqual(true, true);
   });
 
-  it("should have jsdom and jquery setup", () => {
-    assert.strictEqual($("p").text(), "Hello world");
+  it('should have jsdom and jquery setup', () => {
+    assert.strictEqual($('p').text(), 'Hello world');
   });
 });
