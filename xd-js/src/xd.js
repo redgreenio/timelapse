7b12d5f8552c8833ff1507e283065722c4c925da
Ragunath Jawahar <ragunath@redgreen.io>
2021-05-25 11:54:23 +0530

    feat: get identifier from the `data` attribute

--- xd-js/src/xd.js
+++ xd-js/src/xd.js
@@ -2,4 +2,6 @@ const $ = require('jquery');
 
 module.exports = {
   selectOccurrences: (window, identifier) => $(window).find(`[data-identifier='${identifier}']`),
+
+  getIdentifier: (window, htmlElement) => $(window)(htmlElement).data('identifier'),
 };
