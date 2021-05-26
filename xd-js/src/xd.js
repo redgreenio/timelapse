f5411ff9f0a632538f2ce561908cee05cb4e781d
Ragunath Jawahar <ragunath@redgreen.io>
2021-05-26 12:43:17 +0530

    feat: click to select identifiers in their declaration and usage sites

--- xd-js/src/xd.js
+++ xd-js/src/xd.js
@@ -2,7 +2,7 @@ function selectOccurrences(identifier, _$ = $) { return _$.find(`[data-identifie
 
 function getIdentifier(htmlElement, _$ = $) { return _$(htmlElement).data('identifier'); }
 
-function selectSpansWithClass(cssClassName, _$ = $) { return _$(`span.${cssClassName}`).toArray(); }
+function selectSpansWithClass(cssClassName, _$ = $) { return _$(`span.${cssClassName}`); }
 
 function selectMatchingIdentifierSpans(spanElement, _$ = $) {
   const identifier = getIdentifier(spanElement, _$);
@@ -12,10 +12,20 @@ function selectMatchingIdentifierSpans(spanElement, _$ = $) {
 
 function setupListenersForIdentifiers(identifierSpanElements, _$ = $) { // eslint-disable-line no-unused-vars
   identifierSpanElements
+    .toArray()
     .map((spanElement) => _$(spanElement))
     .forEach((span) => {
       span.mouseenter(() => selectMatchingIdentifierSpans(span).addClass('highlight'));
       span.mouseleave(() => selectMatchingIdentifierSpans(span).removeClass('highlight'));
+
+      span.click(() => {
+        const previouslySelectedSpans = _$(selectSpansWithClass('selected'));
+        previouslySelectedSpans.removeClass('selected');
+
+        const matchingIdentifierSpans = selectMatchingIdentifierSpans(span);
+        matchingIdentifierSpans.removeClass('highlight');
+        matchingIdentifierSpans.addClass('selected');
+      });
     });
 }
 
