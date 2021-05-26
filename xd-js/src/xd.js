46ec4b0f2c9af62a67aaa9ce6be8a9859323e913
Ragunath Jawahar <ragunath@redgreen.io>
2021-05-26 11:51:11 +0530

    refactor: rename `jquery` parameter to `_$`

--- xd-js/src/xd.js
+++ xd-js/src/xd.js
@@ -1,19 +1,19 @@
-function selectOccurrences(identifier, jquery = $) { return jquery.find(`[data-identifier='${identifier}']`); }
+function selectOccurrences(identifier, _$ = $) { return _$.find(`[data-identifier='${identifier}']`); }
 
-function getIdentifier(htmlElement, jquery = $) { return jquery(htmlElement).data('identifier'); }
+function getIdentifier(htmlElement, _$ = $) { return _$(htmlElement).data('identifier'); }
 
-function selectSpansWithClass(cssClassName, jquery = $) { return jquery(`span.${cssClassName}`).toArray(); }
+function selectSpansWithClass(cssClassName, _$ = $) { return _$(`span.${cssClassName}`).toArray(); }
 
-function selectMatchingIdentifierSpans(spanElement, jquery = $) {
-  const identifier = getIdentifier(spanElement, jquery);
-  const occurrences = selectOccurrences(identifier, jquery);
-  return jquery(occurrences);
+function selectMatchingIdentifierSpans(spanElement, _$ = $) {
+  const identifier = getIdentifier(spanElement, _$);
+  const occurrences = selectOccurrences(identifier, _$);
+  return _$(occurrences);
 }
 
-function setupListenersForIdentifiers(identifierSpanElements, jquery = $) { // eslint-disable-line no-unused-vars
+function setupListenersForIdentifiers(identifierSpanElements, _$ = $) { // eslint-disable-line no-unused-vars
   identifierSpanElements.forEach((span) => {
-    jquery(span).mouseenter(() => selectMatchingIdentifierSpans(span).addClass('highlight'));
-    jquery(span).mouseleave(() => selectMatchingIdentifierSpans(span).removeClass('highlight'));
+    _$(span).mouseenter(() => selectMatchingIdentifierSpans(span).addClass('highlight'));
+    _$(span).mouseleave(() => selectMatchingIdentifierSpans(span).removeClass('highlight'));
   });
 }
 
