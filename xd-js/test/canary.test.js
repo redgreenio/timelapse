abde93197b13c8e9cb96eca63e63a514d5d7243c
Ragunath Jawahar <ragunath@redgreen.io>
2021-05-26 18:12:26 +0530

    test: add approvals canary test

--- xd-js/test/canary.test.js
+++ xd-js/test/canary.test.js
@@ -1,16 +1,24 @@
-const assert = require('assert');
+const { should } = require('chai');
+
+should();
 
 const { JSDOM } = require('jsdom');
+const approvals = require('approvals');
 
+const approvalsDir = 'test/approved/canary';
 const { window } = new JSDOM('<!DOCTYPE html><p>Hello world</p>');
 const $ = require('jquery')(window);
 
 describe('a test environment', () => {
   it('should have mocha setup', () => {
-    assert.strictEqual(true, true);
+    true.should.be.true; // eslint-disable-line
   });
 
   it('should have jsdom and jquery setup', () => {
-    assert.strictEqual($('p').text(), 'Hello world');
+    $('p').text().should.equal('Hello world');
+  });
+
+  it('should have approvals setup', () => {
+    approvals.verify(approvalsDir, 'should have approvals setup', 'Hello, world!');
   });
 });
