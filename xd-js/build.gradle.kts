ec1f7229a9373669b70a883d631e19cb3ee8aa0f
Ragunath Jawahar <ragunath@redgreen.io>
2021-06-01 12:21:35 +0530

    build: convert Node.Js code to be run inside the browser

--- xd-js/build.gradle.kts
+++ xd-js/build.gradle.kts
@@ -19,3 +19,42 @@ fun registerNpmTask(name: String, command: String, vararg commandArgs: String) {
     args.set(listOf(command, *commandArgs))
   }
 }
+
+tasks.register("build", DefaultTask::class) {
+  description = "Prepares Node.Js code for the browser."
+
+  inputs.files(projectDir.resolve("src/xd.js"))
+  inputs.files(projectDir.resolve("src/static/xd.js.bootstrap"))
+
+  outputs.file(projectDir.resolve("build/xd-browser.js"))
+
+  doLast {
+    val allInputFiles = inputs.files.files.toList()
+    val xdJs = allInputFiles[0]
+    val xdJsBootstrap = allInputFiles[1]
+
+    val browserJs = xdJsBootstrap
+      .readText()
+      .replace("/*{js}*/", padLeft(cleanupNodeJsCode(xdJs.readText()).trim(), "  "))
+
+    val browserJsFile = outputs.files.singleFile
+    browserJsFile.outputStream().use {
+      it.write(browserJs.toByteArray())
+    }
+  }
+}
+
+fun cleanupNodeJsCode(nodeJsSource: String): String {
+  val nodeJsCodeStartIndex = nodeJsSource.indexOf("module.exports")
+  return nodeJsSource.substring(0, nodeJsCodeStartIndex)
+}
+
+fun padLeft(fileContents: String, padding: String): String {
+  val isWindows = System.getProperty("os.name").toLowerCase().contains("windows")
+  val newlineChar = if (isWindows) "\r\n" else "\n"
+
+  return fileContents
+    .trim()
+    .split(newlineChar)
+    .joinToString(newlineChar) { if (it.isNotBlank()) "$padding$it" else it }
+}
