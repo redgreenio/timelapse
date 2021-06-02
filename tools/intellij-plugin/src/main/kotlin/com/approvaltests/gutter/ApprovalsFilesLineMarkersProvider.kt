540d8bc01832bb71308b23c724118a4d13ccedb0
Ragunath Jawahar <ragunath@redgreen.io>
2021-06-02 14:25:02 +0530

    feat: show a single gutter icon to the immediate left of the test function

--- tools/intellij-plugin/src/main/kotlin/com/approvaltests/gutter/ApprovalsFilesLineMarkersProvider.kt
+++ tools/intellij-plugin/src/main/kotlin/com/approvaltests/gutter/ApprovalsFilesLineMarkersProvider.kt
@@ -3,7 +3,7 @@ package com.approvaltests.gutter
 import com.intellij.codeInsight.daemon.LineMarkerInfo
 import com.intellij.codeInsight.daemon.LineMarkerProvider
 import com.intellij.lang.Language
-import com.intellij.openapi.editor.markup.GutterIconRenderer.Alignment.LEFT
+import com.intellij.openapi.editor.markup.GutterIconRenderer.Alignment.RIGHT
 import com.intellij.openapi.util.IconLoader
 import com.intellij.psi.PsiElement
 
@@ -17,8 +17,8 @@ class ApprovalsFilesLineMarkersProvider : LineMarkerProvider {
     }
 
     val isKotlin = element.language.isKindOf(Language.findLanguageByID("kotlin"))
-    return if (isKotlin && element.node.text == "@Test") {
-      LineMarkerInfo(element, element.textRange, icon, null, null, LEFT) { "Approvals test" }
+    return if (isKotlin && element.node.text == "fun") {
+      LineMarkerInfo(element, element.textRange, icon, null, null, RIGHT) { "Approvals test" }
     } else {
       null
     }
