a79e5edd381e1d2c9de0a9d8ffb90e60623b4bc6
Ragunath Jawahar <ragunath@redgreen.io>
2021-06-02 13:07:54 +0530

    test: replace mocking with a `TestVirtualFile`

--- tools/intellij-plugin/src/test/kotlin/com/approvaltests/model/ApprovalFileTest.kt
+++ tools/intellij-plugin/src/test/kotlin/com/approvaltests/model/ApprovalFileTest.kt
@@ -3,9 +3,7 @@ package com.approvaltests.model
 import com.approvaltests.model.ApprovalFile.Approved
 import com.approvaltests.model.ApprovalFile.Received
 import com.google.common.truth.Truth.assertThat
-import com.intellij.openapi.vfs.VirtualFile
-import com.nhaarman.mockitokotlin2.mock
-import com.nhaarman.mockitokotlin2.whenever
+import io.redgreen.intellij.TestVirtualFile
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 
@@ -14,31 +12,19 @@ class ApprovalFileTest {
   inner class FactoryFunction {
     @Test
     fun `create an Approved instance`() {
-      // given
-      val virtualFile = mock<VirtualFile>().apply { whenever(this.name).thenReturn("my.approved.txt") }
-
-      // when & then
-      assertThat(ApprovalFile.from(virtualFile))
+      assertThat(ApprovalFile.from(TestVirtualFile("my.approved.txt")))
         .isInstanceOf(Approved::class.java)
     }
 
     @Test
     fun `create a Received instance`() {
-      // given
-      val virtualFile = mock<VirtualFile>().apply { whenever(this.name).thenReturn("my.received.txt") }
-
-      // when & then
-      assertThat(ApprovalFile.from(virtualFile))
+      assertThat(ApprovalFile.from(TestVirtualFile("my.received.txt")))
         .isInstanceOf(Received::class.java)
     }
 
     @Test
     fun `don't create an instance for files other than approved and received`() {
-      // given
-      val virtualFile = mock<VirtualFile>().apply { whenever(this.name).thenReturn("my-file.txt") }
-
-      // when & then
-      assertThat(ApprovalFile.from(virtualFile))
+      assertThat(ApprovalFile.from(TestVirtualFile("my-file.txt")))
         .isNull()
     }
   }
