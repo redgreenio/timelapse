23a1c3962107dc8149fe2fa0ea4860e28d4ef4f0
Ragunath Jawahar <ragunath@redgreen.io>
2021-06-03 17:54:22 +0530

    chore: show random icon for every launch

--- tools/intellij-plugin/src/main/kotlin/com/approvaltests/markers/ApprovalsFilesLineMarkersProvider.kt
+++ tools/intellij-plugin/src/main/kotlin/com/approvaltests/markers/ApprovalsFilesLineMarkersProvider.kt
@@ -8,10 +8,18 @@ import com.intellij.psi.PsiElement
 import com.intellij.psi.util.PsiTreeUtil
 import org.jetbrains.kotlin.psi.KtDotQualifiedExpression
 import org.jetbrains.kotlin.psi.KtNamedFunction
+import kotlin.random.Random
 
 class ApprovalsFilesLineMarkersProvider : LineMarkerProvider {
   private val revealFeature = true
-  private val icon = IconLoader.getIcon("icons/nonExistent-nonEmpty.svg", this::class.java)
+
+  private val allIcons = listOf(
+    "nonEmpty-empty",
+    "nonEmpty-nonEmpty",
+    "nonEmpty-nonExistent",
+    "nonExistent-nonEmpty",
+    "nonExistent-nonExistent"
+  )
 
   override fun getLineMarkerInfo(element: PsiElement): LineMarkerInfo<*>? {
     if (!revealFeature || element !is KtNamedFunction) {
@@ -20,6 +28,9 @@ class ApprovalsFilesLineMarkersProvider : LineMarkerProvider {
 
     val expression = PsiTreeUtil.findChildOfType(element, KtDotQualifiedExpression::class.java)
     return if (expression != null && isApprovalsVerifyCall(expression)) {
+      val randomIconName = allIcons[Random.nextInt(allIcons.size)]
+      val icon = IconLoader.getIcon("icons/$randomIconName.svg", this::class.java)
+
       val funKeyword = getFunKeyword(element)
       val textRange = funKeyword.textRange
       LineMarkerInfo(element, textRange, icon, null, null, RIGHT) { "Approvals test" }
