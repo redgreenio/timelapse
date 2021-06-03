b0b94bb114b3a98eb28351e4976fb4faf7876dbd
Ragunath Jawahar <ragunath@redgreen.io>
2021-06-03 22:32:24 +0530

    feat: add icons to gutter icon popup menu

--- tools/intellij-plugin/src/main/kotlin/com/approvaltests/markers/actions/ApproveReceivedFile.kt
+++ tools/intellij-plugin/src/main/kotlin/com/approvaltests/markers/actions/ApproveReceivedFile.kt
@@ -1,11 +1,15 @@
 package com.approvaltests.markers.actions
 
+import com.intellij.icons.AllIcons
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 
 class ApproveReceivedFile : AnAction() {
   override fun update(e: AnActionEvent) {
-    e.presentation.text = "Approve Received File"
+    with(e.presentation) {
+      icon = AllIcons.Actions.Commit
+      text = "Approve Received File"
+    }
   }
 
   override fun actionPerformed(e: AnActionEvent) {
--- tools/intellij-plugin/src/main/kotlin/com/approvaltests/markers/actions/ViewApprovedFileAction.kt
+++ tools/intellij-plugin/src/main/kotlin/com/approvaltests/markers/actions/ViewApprovedFileAction.kt
@@ -2,10 +2,16 @@ package com.approvaltests.markers.actions
 
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.util.IconLoader
 
 class ViewApprovedFileAction : AnAction() {
+  private val actionIcon = IconLoader.getIcon("icons/view-approved.svg", this::class.java)
+
   override fun update(e: AnActionEvent) {
-    e.presentation.text = "View Approved File"
+    with(e.presentation) {
+      icon = actionIcon
+      text = "View Approved File"
+    }
   }
 
   override fun actionPerformed(e: AnActionEvent) {
