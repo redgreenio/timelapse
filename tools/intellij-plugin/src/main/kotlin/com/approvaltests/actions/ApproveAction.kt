3586c02a4788647b01c8374f98dd279d1b56939a
Ragunath Jawahar <ragunath@redgreen.io>
2021-06-03 10:22:12 +0530

    [wip]feat: select approved file after completing the approve action

--- tools/intellij-plugin/src/main/kotlin/com/approvaltests/actions/ApproveAction.kt
+++ tools/intellij-plugin/src/main/kotlin/com/approvaltests/actions/ApproveAction.kt
@@ -8,40 +8,54 @@ import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.PlatformDataKeys
 import com.intellij.openapi.command.WriteCommandAction
+import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.editor.Document
+import com.intellij.openapi.fileChooser.FileSystemTree
 import com.intellij.openapi.fileEditor.FileDocumentManager
+import java.io.IOException
 
 class ApproveAction : AnAction() {
+  companion object {
+    private val LOGGER = Logger.getInstance(ApproveAction::class.java)
+  }
+
   override fun update(e: AnActionEvent) {
     val projectPresent = e.project != null
-    val virtualFile = e.dataContext.getData(PlatformDataKeys.VIRTUAL_FILE)
+    val virtualFile = e.getData(PlatformDataKeys.VIRTUAL_FILE)
     val selectedReceivedFile = virtualFile != null && ApprovalFile.from(virtualFile) is Received
 
     e.presentation.isEnabledAndVisible = projectPresent && selectedReceivedFile
   }
 
   override fun actionPerformed(e: AnActionEvent) {
-    val receivedVirtualFile = e.dataContext.getData(PlatformDataKeys.VIRTUAL_FILE)
+    val receivedVirtualFile = e.getData(PlatformDataKeys.VIRTUAL_FILE)
     receivedVirtualFile ?: return
 
     val received = Received(receivedVirtualFile)
     val approved = received.counterpart() as? Approved
 
-    WriteCommandAction.runWriteCommandAction(e.project) { approve(received, approved) }
+    val fileSystemTree = e.getData(FileSystemTree.DATA_KEY)
+
+    WriteCommandAction.runWriteCommandAction(e.project) { approve(received, approved, fileSystemTree) }
   }
 
   private fun approve(
     received: Received,
-    existingApproved: Approved?
+    existingApproved: Approved?,
+    fileSystemTree: FileSystemTree?
   ) {
     if (existingApproved == null) {
       received.virtualFile.rename(this, received.approvedFileName)
     } else {
       val fileDocumentManager = FileDocumentManager.getInstance()
-      val document = fileDocumentManager.getDocument(existingApproved.virtualFile)
-      if (document != null && document.isWritable) {
-        copyReceivedContentToApproved(fileDocumentManager, received, document)
-        deleteReceived(received)
+      val approvedFileDocument = fileDocumentManager.getDocument(existingApproved.virtualFile)
+
+      if (approvedFileDocument != null && approvedFileDocument.isWritable) {
+        copyReceivedContentToApproved(fileDocumentManager, received, approvedFileDocument)
+        fileSystemTree?.select(existingApproved.virtualFile) { fileSystemTree.expand(received.virtualFile, null) }
+        if (!deleteReceived(received)) {
+          fileSystemTree?.select(received.virtualFile) { fileSystemTree.expand(received.virtualFile, null) }
+        }
       }
     }
   }
@@ -58,7 +72,13 @@ class ApproveAction : AnAction() {
     fileDocumentManager.saveDocument(approvedDocument)
   }
 
-  private fun deleteReceived(received: Received) {
-    received.virtualFile.delete(this)
+  private fun deleteReceived(received: Received): Boolean {
+    return try {
+      received.virtualFile.delete(this)
+      true
+    } catch (e: IOException) {
+      LOGGER.trace(e)
+      false
+    }
   }
 }
