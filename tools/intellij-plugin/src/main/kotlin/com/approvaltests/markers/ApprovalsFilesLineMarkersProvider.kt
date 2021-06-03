672deed20ff03e7440999b5e130dfd4e1a8bae15
Ragunath Jawahar <ragunath@redgreen.io>
2021-06-03 17:43:29 +0530

    feat: display approvals markers only for functions with `Approvals.verify` call

--- tools/intellij-plugin/src/main/kotlin/com/approvaltests/markers/ApprovalsFilesLineMarkersProvider.kt
+++ tools/intellij-plugin/src/main/kotlin/com/approvaltests/markers/ApprovalsFilesLineMarkersProvider.kt
@@ -5,6 +5,8 @@ import com.intellij.codeInsight.daemon.LineMarkerProvider
 import com.intellij.openapi.editor.markup.GutterIconRenderer.Alignment.RIGHT
 import com.intellij.openapi.util.IconLoader
 import com.intellij.psi.PsiElement
+import com.intellij.psi.util.PsiTreeUtil
+import org.jetbrains.kotlin.psi.KtDotQualifiedExpression
 import org.jetbrains.kotlin.psi.KtNamedFunction
 
 class ApprovalsFilesLineMarkersProvider : LineMarkerProvider {
@@ -12,11 +14,12 @@ class ApprovalsFilesLineMarkersProvider : LineMarkerProvider {
   private val icon = IconLoader.getIcon("icons/nonExistent-nonEmpty.svg", this::class.java)
 
   override fun getLineMarkerInfo(element: PsiElement): LineMarkerInfo<*>? {
-    if (!revealFeature) {
+    if (!revealFeature || element !is KtNamedFunction) {
       return null
     }
 
-    return if (element is KtNamedFunction) {
+    val expression = PsiTreeUtil.findChildOfType(element, KtDotQualifiedExpression::class.java)
+    return if (expression != null && isApprovalsVerifyCall(expression)) {
       val funKeyword = getFunKeyword(element)
       val textRange = funKeyword.textRange
       LineMarkerInfo(element, textRange, icon, null, null, RIGHT) { "Approvals test" }
