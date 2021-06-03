ec9c8eb4bc1d569e205c712fe30fd14723a495bc
Ragunath Jawahar <ragunath@redgreen.io>
2021-06-03 19:27:50 +0530

    feat: show markers at the beginning for function declaration

--- tools/intellij-plugin/src/main/kotlin/com/approvaltests/markers/ApprovalsFilesLineMarkersProvider.kt
+++ tools/intellij-plugin/src/main/kotlin/com/approvaltests/markers/ApprovalsFilesLineMarkersProvider.kt
@@ -5,7 +5,9 @@ import com.intellij.codeInsight.daemon.LineMarkerProvider
 import com.intellij.openapi.editor.markup.GutterIconRenderer.Alignment.RIGHT
 import com.intellij.openapi.util.IconLoader
 import com.intellij.psi.PsiElement
+import com.intellij.psi.util.PsiTreeUtil
 import org.jetbrains.kotlin.psi.KtDotQualifiedExpression
+import org.jetbrains.kotlin.psi.KtNamedFunction
 import kotlin.random.Random
 
 class ApprovalsFilesLineMarkersProvider : LineMarkerProvider {
@@ -20,11 +22,12 @@ class ApprovalsFilesLineMarkersProvider : LineMarkerProvider {
   )
 
   override fun getLineMarkerInfo(element: PsiElement): LineMarkerInfo<*>? {
-    if (!revealFeature || element !is KtDotQualifiedExpression) {
+    if (!revealFeature || element !is KtNamedFunction) {
       return null
     }
 
-    return if (isApprovalsVerifyCall(element)) {
+    val expression = PsiTreeUtil.findChildOfType(element, KtDotQualifiedExpression::class.java)
+    return if (expression != null && isApprovalsVerifyCall(expression)) {
       val randomIconName = allIcons[Random.nextInt(allIcons.size)]
       val icon = IconLoader.getIcon("icons/$randomIconName.svg", this::class.java)
 
