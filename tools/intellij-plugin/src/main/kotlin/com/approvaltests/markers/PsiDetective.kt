267db207ecf78c7b6808c9bc5312464c8db21fe0
Ragunath Jawahar <ragunath@redgreen.io>
2021-06-03 15:50:42 +0530

    test: `getKtNamedFunction` returns `null` if element is not a child of `KtNamedFunction`

--- tools/intellij-plugin/src/main/kotlin/com/approvaltests/markers/PsiDetective.kt
+++ tools/intellij-plugin/src/main/kotlin/com/approvaltests/markers/PsiDetective.kt
@@ -3,7 +3,7 @@ package com.approvaltests.markers
 import com.intellij.psi.PsiElement
 import org.jetbrains.kotlin.psi.KtNamedFunction
 
-fun getNamedFunction(childElement: PsiElement): KtNamedFunction? {
+fun getKtNamedFunction(childElement: PsiElement): KtNamedFunction? {
   var parent = childElement.parent
   while (parent != null) {
     if (parent is KtNamedFunction) {
