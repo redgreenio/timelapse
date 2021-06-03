e50138bb9fead27a4ba0cd5d30b1e05da1c4e976
Ragunath Jawahar <ragunath@redgreen.io>
2021-06-03 15:42:16 +0530

    feat: get `KtNamedFunction` from a child element

--- tools/intellij-plugin/src/main/kotlin/com/approvaltests/markers/ApprovalsFilesLineMarkersProvider.kt
+++ tools/intellij-plugin/src/main/kotlin/com/approvaltests/markers/ApprovalsFilesLineMarkersProvider.kt
@@ -2,14 +2,13 @@ package com.approvaltests.markers
 
 import com.intellij.codeInsight.daemon.LineMarkerInfo
 import com.intellij.codeInsight.daemon.LineMarkerProvider
-import com.intellij.lang.Language
 import com.intellij.openapi.editor.markup.GutterIconRenderer.Alignment.RIGHT
 import com.intellij.openapi.util.IconLoader
 import com.intellij.psi.PsiElement
 import org.jetbrains.kotlin.psi.KtNamedFunction
 
 class ApprovalsFilesLineMarkersProvider : LineMarkerProvider {
-  private val revealFeature = false
+  private val revealFeature = true
   private val icon = IconLoader.getIcon("icons/nonExistent-nonEmpty.svg", this::class.java)
 
   override fun getLineMarkerInfo(element: PsiElement): LineMarkerInfo<*>? {
@@ -17,9 +16,9 @@ class ApprovalsFilesLineMarkersProvider : LineMarkerProvider {
       return null
     }
 
-    val isKotlin = element.language.isKindOf(Language.findLanguageByID("kotlin"))
-    return if (isKotlin && element is KtNamedFunction) {
-      LineMarkerInfo(element, element.textRange, icon, null, null, RIGHT) { "Approvals test" }
+    return if (element is KtNamedFunction) {
+      val textRange = (element as PsiElement).textRange
+      LineMarkerInfo(element, textRange, icon, null, null, RIGHT) { "Approvals test" }
     } else {
       null
     }
