501b6c436aa2d96498091c3d182448c28178b125
Ragunath Jawahar <ragunath@redgreen.io>
2021-06-04 06:58:50 +0530

    feat: bundle compare with icon within the plugin

--- tools/intellij-plugin/src/main/kotlin/com/approvaltests/markers/actions/CompareReceivedWithApproved.kt
+++ tools/intellij-plugin/src/main/kotlin/com/approvaltests/markers/actions/CompareReceivedWithApproved.kt
@@ -1,13 +1,17 @@
 package com.approvaltests.markers.actions
 
-import com.intellij.icons.AllIcons
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.util.IconLoader
 
 class CompareReceivedWithApproved : AnAction() {
+  companion object {
+    private val compareIcon = IconLoader.getIcon("icons/compareReceivedApproved.svg", this::class.java)
+  }
+
   override fun update(e: AnActionEvent) {
     with(e.presentation) {
-      icon = AllIcons.Actions.Diff
+      icon = compareIcon
       text = "Compare Received <> Approved"
     }
   }
