7e4c0bd655a34bbc45228b41847bd31b93867fef
Ragunath Jawahar <ragunath@redgreen.io>
2021-06-04 21:59:41 +0530

    feat: add parent directory support

--- tools/intellij-plugin/src/test/kotlin/io/redgreen/intellij/FakeVirtualFile.kt
+++ tools/intellij-plugin/src/test/kotlin/io/redgreen/intellij/FakeVirtualFile.kt
@@ -6,16 +6,23 @@ import java.io.InputStream
 import java.io.OutputStream
 
 class FakeVirtualFile private constructor(
-  private val filePath: String
+  private val path: String,
+  private val isDirectory: Boolean = false
 ) : VirtualFile() {
   companion object {
+    private const val FILE_SEPARATOR = "/"
+
     fun fileFromPath(filePath: String): FakeVirtualFile {
       return FakeVirtualFile(filePath)
+    }
+
+    fun directoryFromPath(directoryPath: String): FakeVirtualFile {
+      return FakeVirtualFile(directoryPath, true)
     }
   }
 
   override fun getName(): String {
-    return filePath.split("/").last()
+    return path.split(FILE_SEPARATOR).last()
   }
 
   override fun getFileSystem(): VirtualFileSystem {
@@ -23,7 +30,7 @@ class FakeVirtualFile private constructor(
   }
 
   override fun getPath(): String {
-    TODO("Not yet implemented")
+    return path
   }
 
   override fun isWritable(): Boolean {
@@ -31,15 +38,20 @@ class FakeVirtualFile private constructor(
   }
 
   override fun isDirectory(): Boolean {
-    TODO("Not yet implemented")
+    return isDirectory
   }
 
   override fun isValid(): Boolean {
     TODO("Not yet implemented")
   }
 
-  override fun getParent(): VirtualFile {
-    TODO("Not yet implemented")
+  override fun getParent(): VirtualFile? {
+    val pathSegments = path.split(FILE_SEPARATOR)
+    return if (pathSegments.size == 1) {
+      null
+    } else {
+      directoryFromPath(pathSegments.dropLast(1).joinToString(FILE_SEPARATOR))
+    }
   }
 
   override fun getChildren(): Array<VirtualFile> {
