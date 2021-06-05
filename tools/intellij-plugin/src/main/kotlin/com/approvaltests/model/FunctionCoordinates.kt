a779f6911e69166a99e3facd3ea739c644ff552b
Ragunath Jawahar <ragunath@redgreen.io>
2021-06-05 19:53:00 +0530

    feat: support finding `FunctionCoordinates` for nested classes

--- tools/intellij-plugin/src/main/kotlin/com/approvaltests/model/FunctionCoordinates.kt
+++ tools/intellij-plugin/src/main/kotlin/com/approvaltests/model/FunctionCoordinates.kt
@@ -1,23 +1,27 @@
 package com.approvaltests.model
 
 class FunctionCoordinates private constructor(
-  private val functionName: String,
-  private val containingClass: String
+  private val identifier: String,
+  private val containingClasses: List<String>
 ) {
   companion object {
     fun from(
       functionName: String,
-      containingClass: String
+      containingClass: String,
+      vararg containingClasses: String
     ): FunctionCoordinates {
-      return FunctionCoordinates(functionName, containingClass)
+      return FunctionCoordinates(functionName, listOf(*containingClasses, containingClass))
     }
   }
 
-  fun bestGuessApprovedFileName(): String {
-    return listOf(containingClass, functionName, "approved", "txt").joinToString(".")
-  }
+  fun bestGuessApprovedFileName(): String =
+    fileNameFor("approved")
 
-  fun bestGuessReceivedFileName(): String {
-    return listOf(containingClass, functionName, "received", "txt").joinToString(".")
+  fun bestGuessReceivedFileName(): String =
+    fileNameFor("received")
+
+  private fun fileNameFor(approvalFileType: String): String {
+    val listOf = containingClasses + listOf(identifier, approvalFileType, "txt")
+    return listOf.joinToString(".")
   }
 }
