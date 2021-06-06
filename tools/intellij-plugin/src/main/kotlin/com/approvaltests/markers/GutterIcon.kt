f17b9c85a7b646f48168fcba9097cd708b81afad
Ragunath Jawahar <ragunath@redgreen.io>
2021-06-06 20:36:43 +0530

    feat: map icon resources to appropriate enumerated values

--- tools/intellij-plugin/src/main/kotlin/com/approvaltests/markers/GutterIcon.kt
+++ tools/intellij-plugin/src/main/kotlin/com/approvaltests/markers/GutterIcon.kt
@@ -1,10 +1,29 @@
 package com.approvaltests.markers
 
 enum class GutterIcon {
-  MISSING_MISSING,
-  MISSING_PRESENT,
-  PRESENT_MISSING,
-  PRESENT_EMPTY,
-  PRESENT_PRESENT_SAME,
-  PRESENT_PRESENT_DIFFERENT,
+  MISSING_MISSING {
+    override val iconResourceName: String = "missing-missing"
+  },
+
+  MISSING_PRESENT {
+    override val iconResourceName: String = "missing-present"
+  },
+
+  PRESENT_MISSING {
+    override val iconResourceName: String = "present-missing"
+  },
+
+  PRESENT_EMPTY {
+    override val iconResourceName: String = "present-empty"
+  },
+
+  PRESENT_PRESENT_SAME {
+    override val iconResourceName: String = "present-present-same"
+  },
+
+  PRESENT_PRESENT_DIFFERENT {
+    override val iconResourceName: String = "present-present-different"
+  };
+
+  abstract val iconResourceName: String
 }
