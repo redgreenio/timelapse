31ab0238bd416700754bcd1ee895fe2801901e75
Ragunath Jawahar <ragunath@redgreen.io>
2021-06-07 06:47:47 +0530

    refactor: replace dark icon nested tests with a parameterized test

--- tools/intellij-plugin/src/test/kotlin/com/approvaltests/markers/GutterIconTest.kt
+++ tools/intellij-plugin/src/test/kotlin/com/approvaltests/markers/GutterIconTest.kt
@@ -1,18 +1,10 @@
 package com.approvaltests.markers
 
-import com.approvaltests.markers.GutterIcon.MISSING_MISSING
-import com.approvaltests.markers.GutterIcon.MISSING_PRESENT
-import com.approvaltests.markers.GutterIcon.PRESENT_EMPTY
-import com.approvaltests.markers.GutterIcon.PRESENT_MISSING
-import com.approvaltests.markers.GutterIcon.PRESENT_PRESENT_DIFFERENT
-import com.approvaltests.markers.GutterIcon.PRESENT_PRESENT_SAME
 import org.approvaltests.Approvals
 import org.approvaltests.core.Options
 import org.approvaltests.namer.NamerFactory
 import org.approvaltests.writers.ApprovalTextWriter
 import org.junit.jupiter.api.AfterEach
-import org.junit.jupiter.api.Nested
-import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.MethodSource
 
@@ -38,37 +30,11 @@ class GutterIconTest {
     Approvals.verify(ApprovalTextWriter(svgResource(icon), svgOptions))
   }
 
-  @Nested
-  inner class DarkIcon {
-    @Test
-    fun `missing missing`() {
-      Approvals.verify(svgResourceDark(MISSING_MISSING), svgOptions)
-    }
-
-    @Test
-    fun `missing present`() {
-      Approvals.verify(svgResourceDark(MISSING_PRESENT), svgOptions)
-    }
-
-    @Test
-    fun `present missing`() {
-      Approvals.verify(svgResourceDark(PRESENT_MISSING), svgOptions)
-    }
-
-    @Test
-    fun `present empty`() {
-      Approvals.verify(svgResourceDark(PRESENT_EMPTY), svgOptions)
-    }
-
-    @Test
-    fun `present present (same)`() {
-      Approvals.verify(svgResourceDark(PRESENT_PRESENT_SAME), svgOptions)
-    }
-
-    @Test
-    fun `present present (different)`() {
-      Approvals.verify(svgResourceDark(PRESENT_PRESENT_DIFFERENT), svgOptions)
-    }
+  @ParameterizedTest
+  @MethodSource("gutterIconEnumValues")
+  fun `dark icon`(icon: GutterIcon) {
+    NamerFactory.additionalInformation = icon.name
+    Approvals.verify(ApprovalTextWriter(svgResourceDark(icon), svgOptions))
   }
 
   private fun svgResource(gutterIcon: GutterIcon): String {
