55059d62a14796e3d62e7b879de8cc2a227528da
Ragunath Jawahar <ragunath@redgreen.io>
2021-06-08 04:21:17 +0530

    feat(plugin): implement "View Approved File" action

--- tools/intellij-plugin/CHANGELOG.md
+++ tools/intellij-plugin/CHANGELOG.md
@@ -2,14 +2,21 @@
 
 ## [Unreleased]
 
+### Added
+
+- Add approval tests gutter icon with compare, view (approved/received), and approve actions. 
 - macOS and Windows shortcuts for "Approve File" and "Compare With Approved/Received File" actions.
 - Add an "Approve File" action to Project View popup menu.
 - Add a "Compare with ..." action to Project View popup menu.
 
 ## [0.0.2] - 2021-05-25
 
+### Added
+
 - Add a live template to deal with identifiers.
 
 ## [0.0.1] - 2021-05-18
+
+### Added
 
 - Add the first set of live templates for XD.
--- tools/intellij-plugin/src/main/kotlin/com/approvaltests/markers/actions/ViewApprovedFile.kt
+++ tools/intellij-plugin/src/main/kotlin/com/approvaltests/markers/actions/ViewApprovedFile.kt
@@ -1,10 +1,16 @@
 package com.approvaltests.markers.actions
 
+import com.approvaltests.model.FunctionCoordinates
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.PlatformDataKeys
+import com.intellij.openapi.fileEditor.FileEditorManager
 import com.intellij.openapi.util.IconLoader
 
-class ViewApprovedFile(private val isRelevant: Boolean) : AnAction() {
+class ViewApprovedFile(
+  private val coordinates: FunctionCoordinates,
+  private val isRelevant: Boolean
+) : AnAction() {
   private val actionIcon = IconLoader.getIcon("icons/view-approved.svg", this::class.java)
 
   override fun update(e: AnActionEvent) {
@@ -16,6 +22,12 @@ class ViewApprovedFile(private val isRelevant: Boolean) : AnAction() {
   }
 
   override fun actionPerformed(e: AnActionEvent) {
-    TODO("Not yet implemented")
+    val testFileDirectory = e.getData(PlatformDataKeys.VIRTUAL_FILE)!!.parent
+    val approvedFile = testFileDirectory.children
+      .find { it.name.startsWith(coordinates.bestGuessApprovedFileNamePrefix()) }
+
+    approvedFile?.let {
+      FileEditorManager.getInstance(e.project!!).openFile(it, true)
+    }
   }
 }
