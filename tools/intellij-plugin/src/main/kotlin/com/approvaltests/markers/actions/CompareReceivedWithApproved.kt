9e1a7ebec3e434c5568b21be0c2790da4e882908
Ragunath Jawahar <ragunath@redgreen.io>
2021-06-08 04:52:36 +0530

    feat(plugin): implement "Compare Received <> Approved" action

--- tools/intellij-plugin/src/main/kotlin/com/approvaltests/markers/actions/CompareReceivedWithApproved.kt
+++ tools/intellij-plugin/src/main/kotlin/com/approvaltests/markers/actions/CompareReceivedWithApproved.kt
@@ -1,10 +1,19 @@
 package com.approvaltests.markers.actions
 
+import com.approvaltests.intellij.compare
+import com.approvaltests.model.ApprovalFile
+import com.approvaltests.model.ApprovalFile.Approved
+import com.approvaltests.model.ApprovalFile.Received
+import com.approvaltests.model.FunctionCoordinates
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.PlatformDataKeys
 import com.intellij.openapi.util.IconLoader
 
-class CompareReceivedWithApproved(private val isRelevant: Boolean) : AnAction() {
+class CompareReceivedWithApproved(
+  private val coordinates: FunctionCoordinates,
+  private val isRelevant: Boolean
+) : AnAction() {
   companion object {
     private val compareIcon = IconLoader.getIcon("icons/compareReceivedApproved.svg", this::class.java)
   }
@@ -18,6 +27,13 @@ class CompareReceivedWithApproved(private val isRelevant: Boolean) : AnAction() 
   }
 
   override fun actionPerformed(e: AnActionEvent) {
-    TODO("Not yet implemented")
+    val testFileDirectory = e.getData(PlatformDataKeys.VIRTUAL_FILE)!!.parent
+    val approvedFile = testFileDirectory.children
+      .find { it.name.startsWith(coordinates.bestGuessApprovedFileNamePrefix()) }
+
+    val approved = ApprovalFile.from(approvedFile!!) as Approved
+    val received = approved.counterpart() as Received
+
+    compare(e.project!!, received, approved)
   }
 }
