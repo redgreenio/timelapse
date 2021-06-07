0bced974ab522e26560ea503b3451c6e3c9f026c
Ragunath Jawahar <ragunath@redgreen.io>
2021-06-08 02:05:04 +0530

    refactor: use `NamerFactory` correctly

--- tools/intellij-plugin/src/test/kotlin/com/approvaltests/markers/GutterIconTest.kt
+++ tools/intellij-plugin/src/test/kotlin/com/approvaltests/markers/GutterIconTest.kt
@@ -16,7 +16,6 @@ import org.approvaltests.Approvals
 import org.approvaltests.core.Options
 import org.approvaltests.namer.NamerFactory
 import org.approvaltests.writers.ApprovalTextWriter
-import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.MethodSource
 
@@ -58,23 +57,20 @@ class GutterIconTest {
     }
   }
 
-  @AfterEach
-  fun tearDown() {
-    NamerFactory.additionalInformation = null
-  }
-
   @ParameterizedTest
   @MethodSource("gutterIconEnumValues")
   fun `light icon`(icon: GutterIcon) {
-    NamerFactory.additionalInformation = icon.name
-    Approvals.verify(ApprovalTextWriter(svgResource(icon), svgOptions))
+    NamerFactory.withParameters(icon.name).use {
+      Approvals.verify(ApprovalTextWriter(svgResource(icon), svgOptions))
+    }
   }
 
   @ParameterizedTest
   @MethodSource("gutterIconEnumValues")
   fun `dark icon`(icon: GutterIcon) {
-    NamerFactory.additionalInformation = icon.name
-    Approvals.verify(ApprovalTextWriter(svgResourceDark(icon), svgOptions))
+    NamerFactory.withParameters(icon.name).use {
+      Approvals.verify(ApprovalTextWriter(svgResourceDark(icon), svgOptions))
+    }
   }
 
   @ParameterizedTest
