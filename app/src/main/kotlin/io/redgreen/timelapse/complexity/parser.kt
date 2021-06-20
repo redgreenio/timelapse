638c5fc63049d31d65658ebdc613808d8afd8f10
Ragunath Jawahar <ragunath@redgreen.io>
2021-06-20 09:11:12 +0530

    fix: handle file moves

--- app/src/main/kotlin/io/redgreen/timelapse/complexity/parser.kt
+++ app/src/main/kotlin/io/redgreen/timelapse/complexity/parser.kt
@@ -9,29 +9,9 @@ private const val RAW_COMMIT_LINE_COUNT = 5
 private const val NEWLINE = "\n"
 private const val TAB = "\t"
 private const val SPACE = " "
-
-/**
- * Parser for command,
- * git log --pretty='%H |@| %aN |@| %ae |@| %aI%n%cN |@| %ce |@| %cI%n%s' --numstat --follow -- <file-path>
- */
-internal fun parse(rawCommit: String): ParsedCommit {
-  val lines = rawCommit.lines()
-  val (sha, authorName, authorEmail, authoredIso) = lines[0].split(DELIMITER)
-  val (committerName, committerEmail, committedIso) = lines[1].split(DELIMITER)
-  val summary = lines[2]
-  val (insertions, deletions, filePath) = sanitizeStatsLine(lines.last()).split(" ")
-
-  return ParsedCommit(
-    CommitHash(sha),
-    summary,
-    Identity(authorName, authorEmail),
-    ZonedDateTime.parse(authoredIso),
-    Identity(committerName, committerEmail),
-    ZonedDateTime.parse(committedIso),
-    filePath,
-    Stats(insertions.toInt(), deletions.toInt())
-  )
-}
+private const val OPEN_CURLY = "{"
+private const val ARROW = " => "
+private const val CLOSE_CURLY = "}"
 
 fun parseAll(rawCommits: String): List<ParsedCommit> {
   val allLines = rawCommits.lines()
@@ -42,9 +22,64 @@ fun parseAll(rawCommits: String): List<ParsedCommit> {
   return splitRawCommits.map(::parse)
 }
 
+/**
+ * Parser for command,
+ * git log --pretty='%H |@| %aN |@| %ae |@| %aI%n%cN |@| %ce |@| %cI%n%s' --numstat --follow -- <file-path>
+ */
+internal fun parse(rawCommit: String): ParsedCommit {
+  val lines = rawCommit.lines()
+  val (sha, authorName, authorEmail, authoredIso) = lines[0].split(DELIMITER)
+  val (committerName, committerEmail, committedIso) = lines[1].split(DELIMITER)
+  val summary = lines[2]
+  val (insertions, deletions, filePath) = statsAndFilePath(lines.last())
+
+  return ParsedCommit(
+    CommitHash(sha),
+    summary,
+    Identity(authorName, authorEmail),
+    ZonedDateTime.parse(authoredIso),
+    Identity(committerName, committerEmail),
+    ZonedDateTime.parse(committedIso),
+    filePath,
+    Stats(insertions, deletions)
+  )
+}
+
+private fun statsAndFilePath(statsLine: String): Triple<Int, Int, String> {
+  val sanitizedStatsLine = sanitizeStatsLine(statsLine)
+  val firstSpaceCharIndex = sanitizedStatsLine.indexOf(SPACE)
+  val secondSpaceCharIndex = sanitizedStatsLine.indexOf(SPACE, firstSpaceCharIndex + 1)
+
+  val insertions = sanitizedStatsLine.substring(0, firstSpaceCharIndex).toInt()
+  val deletions = sanitizedStatsLine.substring(firstSpaceCharIndex + 1, secondSpaceCharIndex).toInt()
+  val filePath = getFilePath(sanitizedStatsLine, secondSpaceCharIndex)
+
+  return Triple(insertions, deletions, filePath)
+}
+
 private fun sanitizeStatsLine(statsLine: String): String {
   return statsLine.trim()
     .replace(Regex(" +"), SPACE) // Multiple spaces with a single space
     .replaceFirst(TAB, SPACE) // First tab character with a space
     .replaceFirst(TAB, SPACE) // Second tab character with a space
+}
+
+private fun getFilePath(sanitizedStatsLine: String, filePathBeginIndex: Int): String {
+  val filePathInformation = sanitizedStatsLine.substring(filePathBeginIndex + 1)
+
+  val openCurlyIndex = filePathInformation.indexOf(OPEN_CURLY)
+  val arrowIndex = filePathInformation.indexOf(ARROW)
+  val closeCurlyIndex = filePathInformation.indexOf(CLOSE_CURLY)
+
+  val renamedFile = arrowIndex in (openCurlyIndex + 1) until closeCurlyIndex
+
+  return if (renamedFile) {
+    val (_, newSegment) = filePathInformation.substring(openCurlyIndex, closeCurlyIndex).split(ARROW)
+
+    filePathInformation.substring(0, openCurlyIndex) +
+      newSegment +
+      filePathInformation.substring(closeCurlyIndex + 1)
+  } else {
+    filePathInformation
+  }
 }
--- app/src/test/kotlin/io/redgreen/timelapse/complexity/ParserTest.kt
+++ app/src/test/kotlin/io/redgreen/timelapse/complexity/ParserTest.kt
@@ -8,6 +8,8 @@ import java.time.ZoneOffset
 import java.time.ZonedDateTime
 
 class ParserTest {
+  private val istOffset = ZoneOffset.of("+05:30")
+
   @Test
   fun `it should parse commit text with short commit message`() {
     // given
@@ -27,9 +29,9 @@ class ParserTest {
       CommitHash("cdf75934bad11807964253b0c74dac2f8d666be5"),
       "refactor: rename classes",
       Identity("Ragunath Jawahar", "ragunath@redgreen.io"),
-      ZonedDateTime.of(2021, 6, 20, 5, 38, 41, 0, ZoneOffset.of("+05:30")),
+      ZonedDateTime.of(2021, 6, 20, 5, 38, 41, 0, istOffset),
       Identity("Ragunath Jawahar", "ragunath@redgreen.io"),
-      ZonedDateTime.of(2021, 6, 20, 5, 38, 41, 0, ZoneOffset.of("+05:30")),
+      ZonedDateTime.of(2021, 6, 20, 5, 38, 41, 0, istOffset),
       "cli/src/main/kotlin/io/redgreen/fluid/cli/ui/Printer.kt",
       Stats(0, 7)
     )
@@ -63,17 +65,15 @@ class ParserTest {
     val commits = parseAll(rawCommits)
 
     // then
-    val zoneOffset = ZoneOffset.of("+05:30")
-
     assertThat(commits)
       .containsExactly(
         ParsedCommit(
           CommitHash("cdf75934bad11807964253b0c74dac2f8d666be5"),
           "refactor: rename classes",
           Identity("Ragunath Jawahar", "ragunath@redgreen.io"),
-          ZonedDateTime.of(2021, 6, 20, 5, 38, 41, 0, zoneOffset),
+          ZonedDateTime.of(2021, 6, 20, 5, 38, 41, 0, istOffset),
           Identity("Ragunath Jawahar", "ragunath@redgreen.io"),
-          ZonedDateTime.of(2021, 6, 20, 5, 38, 41, 0, zoneOffset),
+          ZonedDateTime.of(2021, 6, 20, 5, 38, 41, 0, istOffset),
           "cli/src/main/kotlin/io/redgreen/fluid/cli/ui/Printer.kt",
           Stats(0, 7)
         ),
@@ -82,9 +82,9 @@ class ParserTest {
           CommitHash("87e7d3e664d0e0a17d417903528a67641be66048"),
           "feat: ask for user's confirmation before overwriting",
           Identity("Ragunath Jawahar", "ragunath@redgreen.io"),
-          ZonedDateTime.of(2020, 4, 17, 18, 44, 0, 0, zoneOffset),
+          ZonedDateTime.of(2020, 4, 17, 18, 44, 0, 0, istOffset),
           Identity("Ragunath Jawahar", "ragunath@redgreen.io"),
-          ZonedDateTime.of(2020, 4, 19, 21, 54, 28, 0, zoneOffset),
+          ZonedDateTime.of(2020, 4, 19, 21, 54, 28, 0, istOffset),
           "cli/src/main/kotlin/io/redgreen/fluid/cli/ui/Printer.kt",
           Stats(2, 2)
         ),
@@ -93,9 +93,9 @@ class ParserTest {
           CommitHash("e119c5d14a7634c2f8478e92c5bb40a88feff3d4"),
           "feat: use the 'install' command to perform a fresh install",
           Identity("Ragunath Jawahar", "ragunath@redgreen.io"),
-          ZonedDateTime.of(2020, 4, 13, 20, 14, 29, 0, zoneOffset),
+          ZonedDateTime.of(2020, 4, 13, 20, 14, 29, 0, istOffset),
           Identity("Ragunath Jawahar", "ragunath@redgreen.io"),
-          ZonedDateTime.of(2020, 4, 13, 22, 3, 44, 0, zoneOffset),
+          ZonedDateTime.of(2020, 4, 13, 22, 3, 44, 0, istOffset),
           "cli/src/main/kotlin/io/redgreen/fluid/cli/ui/Printer.kt",
           Stats(7, 0)
         )
@@ -120,11 +120,41 @@ class ParserTest {
       CommitHash("cdf75934bad11807964253b0c74dac2f8d666be5"),
       "refactor: rename classes",
       Identity("Ragunath Jawahar", "ragunath@redgreen.io"),
-      ZonedDateTime.of(2021, 6, 20, 5, 38, 41, 0, ZoneOffset.of("+05:30")),
+      ZonedDateTime.of(2021, 6, 20, 5, 38, 41, 0, istOffset),
       Identity("Ragunath Jawahar", "ragunath@redgreen.io"),
-      ZonedDateTime.of(2021, 6, 20, 5, 38, 41, 0, ZoneOffset.of("+05:30")),
+      ZonedDateTime.of(2021, 6, 20, 5, 38, 41, 0, istOffset),
       "app/src/main/java/org/simple/clinic/newentry/PatientEntryScreenController.kt",
       Stats(0, 207)
+    )
+
+    assertThat(actualCommit)
+      .isEqualTo(expectedCommit)
+  }
+
+  @Test
+  fun `it should parse a commit with a renamed file`() {
+    // given
+    val rawCommit = """
+      b065618bf24c7876a7f4e165c41e7df2bd396731 |@| John Doe |@| john@somewhere.in |@| 2018-06-15T20:02:04+05:30
+      Hans Solo |@| hans@somewhere.in |@| 2018-06-15T20:57:38+05:30
+      Change package name to org.simple.clinic
+
+      15	15	app/src/main/java/org/{resolvetosavelives/red => simple/clinic}/newentry/PatientEntryScreenController.kt
+    """.trimIndent()
+
+    // when
+    val actualCommit = parse(rawCommit)
+
+    // then
+    val expectedCommit = ParsedCommit(
+      CommitHash("b065618bf24c7876a7f4e165c41e7df2bd396731"),
+      "Change package name to org.simple.clinic",
+      Identity("John Doe", "john@somewhere.in"),
+      ZonedDateTime.of(2018, 6, 15, 20, 2, 4, 0, istOffset),
+      Identity("Hans Solo", "hans@somewhere.in"),
+      ZonedDateTime.of(2018, 6, 15, 20, 57, 38, 0, istOffset),
+      "app/src/main/java/org/simple/clinic/newentry/PatientEntryScreenController.kt",
+      Stats(15, 15)
     )
 
     assertThat(actualCommit)
